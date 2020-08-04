6cea68ed989b95d7dc4d845d5497c5d407c4e099
jlu255 <jlu255@wisc.edu>
2020-08-03 23:24:03 -0400

    set up scss

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -29,22 +29,9 @@ export interface IButtonProps {
 }
 
 export const Card = () => {
-  const style = {
-    width: "18rem",
-    height: "23rem",
-    border: "solid",
-    borderWidth: "thin",
-    borderRadius: "5px",
-    borderColor: "grey",
-  };
   return (
-    <div className="card" style={style}>
-      <img
-        className="card-img-top"
-        src="https://via.placeholder.com/150"
-        width="280px"
-        height="160px"
-      />
+    <div className="card">
+      <img className="card-img-top" src="https://via.placeholder.com/150" />
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">

300019255c879f496165a0cf88861e0e1553a3c0
uranussg <uranussg@gmail.com>
2020-08-06 15:29:04 -0400

    update

--- src/components/Rating/Rating.test.tsx
+++ src/components/Rating/Rating.test.tsx
@@ -3,6 +3,7 @@ import {render, fireEvent} from '@testing-library/react';
 import Rating from './Rating'
 
 
+
 describe('Rating', () => {
     it('should render default stars', () => {
         const wrapper = render(<Rating />)

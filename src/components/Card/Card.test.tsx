fda5758b7dbaaef3284a5336bd431c0b2d1d2432
jlu255 <jlu255@wisc.edu>
2020-08-06 18:46:19 -0400

    complete test

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,21 +1,48 @@
 import React from 'react';
-
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import Card, { CardType, CardSize } from './Card';
 describe('Card', () => {
   it('should render default card', () => {
     const cardProps = {
-      //   onClick: jest.fn()
+      onClick: jest.fn(),
+    };
+    const wrapper = render(<Card {...cardProps}></Card>);
+    const cardElement = screen.getByTestId('card-element');
+    expect(cardElement).toHaveClass('card card-default');
+  });
+  it('should render dark card with correct title', () => {
+    const cardProps = {
       cardType: CardType.Dark,
+      onClick: jest.fn(),
     };
     const wrapper = render(<Card {...cardProps}>Dark Card</Card>);
-    const element = wrapper.queryByText('Dark Card') as HTMLElement;
-    expect(element).toBeInTheDocument();
-    expect(element.tagName).toBe('H5');
-    const cardElement = wrapper.container.firstChild;
+    //test h5 title
+    const titleElement = wrapper.queryByText('Dark Card') as HTMLElement;
+    expect(titleElement).toBeInTheDocument();
+    expect(titleElement.tagName).toBe('H5');
+    //test class name
+    const cardElement = screen.getByTestId('card-element');
     expect(cardElement).toHaveClass('card card-dark');
-    // expect(cardProps.onClick).toHaveBeenCalledTimes(0);
-    // fireEvent.click(element);
-    // expect(cardProps.onClick).toHaveBeenCalledTimes(1);
+  });
+  it('should render small size card', () => {
+    const cardProps = {
+      cardSize: CardSize.Small,
+      onClick: jest.fn(),
+    };
+    const wrapper = render(<Card {...cardProps}>Small Card</Card>);
+    const cardElement = screen.getByTestId('card-element');
+    expect(cardElement).toHaveClass('card card-default card-sm');
+  });
+  it('should render button can be clicked', () => {
+    const cardProps = {
+      onClick: jest.fn(),
+    };
+    //test button
+    const wrapper = render(<Card {...cardProps}></Card>);
+    const buttonElement = screen.getByTestId('button-element');
+    expect(buttonElement.tagName).toBe('BUTTON');
+    expect(cardProps.onClick).toHaveBeenCalledTimes(0);
+    fireEvent.click(buttonElement);
+    expect(cardProps.onClick).toHaveBeenCalledTimes(1);
   });
 });
--- src/components/Card/myCard.js
+++ src/components/Card/myCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from '../Button/Button';
 function MyCard(props) {
   return (
-    <div className={props.className}>
+    <div className={props.className} data-testid='card-element'>
       <div className={props.className + ' image'}>
         <img src={props.src} />
       </div>
@@ -12,7 +12,12 @@ function MyCard(props) {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </p>
-        <Button onClick={props.onClick} className='btn btn-primary' href='#'>
+        <Button
+          onClick={props.onClick}
+          data-testid='button-element'
+          className='btn btn-primary'
+          href='#'
+        >
           Show More Info
         </Button>
       </div>

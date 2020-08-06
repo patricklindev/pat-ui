612997fc40ff64b6949365311a3a1dd10a385a04
Chen Yang <chen@pporn.tv>
2020-08-06 14:33:01 -0400

    Add storybook for dropdown

---
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {action} from '@storybook/addon-actions';
+import Dropdown from './Dropdown';
+
+export default {
+  title: 'Dropdown',
+  component: Dropdown,
+};
+
+export const DefaultDropdown = () => (
+  <div>
+    <Dropdown onChange={action(`selected`)} placeholder='Select a type'>
+      <Dropdown.Item>Sedan</Dropdown.Item>
+      <Dropdown.Item>SUV</Dropdown.Item>
+      <Dropdown.Item>Convertible</Dropdown.Item>
+    </Dropdown>
+  </div>
+);

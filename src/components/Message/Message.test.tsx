643ced6e1aead066d86b3a9a86cba2c68b2c6edc
Lynn Dai <lingyundaiseven@gmail.com>
2020-08-07 06:43:14 -0400

    added more test cases

--- src/components/Message/Message.test.tsx
+++ src/components/Message/Message.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {render, fireEvent} from '@testing-library/react';
-import Message, {messageType} from './Message';
+import Message, {messageType, iconType} from './Message';
 import { ButtonType } from '../Button/Button';
 
 describe('Message', () => {
@@ -11,6 +11,7 @@ describe('Message', () => {
     });
 
     it('should render correct message based on different props', () => {
+        // Basic message
         const msgBasicProps = {
             msgType: messageType.Basic,
             className: 'test'
@@ -21,18 +22,25 @@ describe('Message', () => {
         const msgBasicElement = msgBasicWrapper.queryByText('Basic Message') as HTMLElement;
         expect(msgBasicElement).toBeInTheDocument();
         expect(msgBasicElement).toHaveClass('msg msg-bs test');
+
+        // List message
+
+        // Icon message
+
+        // Dismiss message
+        const msgDismissProps = {
+            msgType: messageType.Dismiss,
+            onClick: jest.fn(),
+            iconType: iconType.Remove
+        }
+        const msgDismissWrapper = render(
+            <Message {...msgDismissProps}>Dismiss Message</Message>
+        );
+        const msgDismissElement = msgDismissWrapper.queryByText('Dismiss Message') as HTMLElement;
+        expect(msgDismissElement).toBeInTheDocument();
+        expect(msgDismissElement).toHaveClass('msg msg-ds msg-remove');
+        expect(msgDismissProps.onClick).toHaveBeenCalledTimes(0);
+        fireEvent.click(msgDismissElement);
+        expect(msgDismissProps.onClick).toHaveBeenCalledTimes(1);
     });
 });
-
-// const btnPrimarySmallWrapper = render(
-//     <Button {...btnPrimarySmallProps}>Primary Small Button</Button>
-//   );
-//   const btnPrimarySmallElement = btnPrimarySmallWrapper.queryByText(
-//     'Primary Small Button'
-//   ) as HTMLElement;
-//   expect(btnPrimarySmallElement).toBeInTheDocument();
-//   expect(btnPrimarySmallElement.tagName).toBe('BUTTON');
-//   expect(btnPrimarySmallElement).toHaveClass('btn btn-primary btn-sm test');
-//   expect(btnPrimarySmallProps.onClick).toHaveBeenCalledTimes(0);
-//   fireEvent.click(btnPrimarySmallElement);
-//   expect(btnPrimarySmallProps.onClick).toHaveBeenCalledTimes(1);

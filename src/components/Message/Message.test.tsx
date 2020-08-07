b79734dbdd2502f7c02b0caf26aa0b97bf544f32
Lynn Dai <lingyundaiseven@gmail.com>
2020-08-06 20:10:55 -0400

    added test for message

---
+++ src/components/Message/Message.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {render, fireEvent} from '@testing-library/react';
+import Message, {messageType} from './Message';
+
+describe('Message', () => {
+    it('should match snapshot', () => {
+      const {asFragment} = render(<Message> Snapshot Message </Message>);
+      expect(asFragment()).toMatchSnapshot();
+    });
+    
+});
+

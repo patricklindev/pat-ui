b13dc306e6804cfdc4365ab6a5da83ae54db76a0
jlu255 <jlu255@wisc.edu>
2020-08-06 21:00:36 -0400

    change style

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import Card, { CardType, CardSize } from './Card';
+import Card from './Card';
 
 export default {
   title: 'Card',

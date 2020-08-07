076e12cc2243be45a6255ba63bb6abaa6942ebd1
Esther Wei <estherwei@Esthers-MacBook-Pro.local>
2020-08-06 23:07:32 -0400

    done

--- src/components/Tabs/Tabs.test.tsx
+++ src/components/Tabs/Tabs.test.tsx
@@ -5,7 +5,7 @@ import { render } from '@testing-library/react';
 import Tabs from '../Tabs/Tabs';
 
 describe('Tabs', () => {
-  it('should render default tabs', () => {
+  it('should render tab1', () => {
     const wrapper = render(<Tabs headings={['Tab1', 'Tab2']}></Tabs>);
     const element = wrapper.queryByText('Tab1') as HTMLElement;
     expect(element).toBeInTheDocument();

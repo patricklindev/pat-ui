baf47965e289f75ad0044f758aa6a14ed2ab376e
Esther Wei <estherwei@Esthers-MacBook-Pro.local>
2020-08-09 22:27:56 -0400

    edited test case

--- src/components/Tabs/Tabs.test.tsx
+++ src/components/Tabs/Tabs.test.tsx
@@ -7,7 +7,10 @@ import Tabs from '../Tabs/Tabs';
 describe('Tabs', () => {
   it('should match snapshot', () => {
     const { asFragment } = render(
-      <Tabs headings={['t1', 't2']}> Snapshot Tabs </Tabs>
+      <Tabs headings={['t1', 't2']} content={['This is Tab1', 'This is Tab2']}>
+        {' '}
+        Snapshot Tabs{' '}
+      </Tabs>
     );
     expect(asFragment()).toMatchSnapshot();
   });
@@ -15,7 +18,9 @@ describe('Tabs', () => {
     const liProps = {
       onClick: jest.fn(),
     };
-    const wrapper = render(<Tabs headings={['Tab1', 'Tab2']}></Tabs>);
+    const wrapper = render(
+      <Tabs headings={['Tab1', 'Tab2']} content={['p1', 'p2']}></Tabs>
+    );
     const element = wrapper.queryByText('Tab1') as HTMLElement;
     expect(element).toBeInTheDocument();
     expect(element.tagName).toBe('LI');

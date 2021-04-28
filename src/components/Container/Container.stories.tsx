3d77a0a634bf5d535acfb9a51e6f8e50cea9ae66
Tieulam Thai <ttl.t954@gmail.com>
2021-04-27 21:52:38 -0600

    add: storybook

--- src/components/Container/Container.stories.tsx
+++ src/components/Container/Container.stories.tsx
@@ -1,3 +1,16 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import Container from './Container';
+
+export default {
+    title: 'Container',
+    component: Container,
+}
+
+export const DefaultContainer = () => {
+    <Container>
+        <p>child 1</p>
+        <p>child 2</p>
+        <p>child 3</p>
+    </Container>
+}

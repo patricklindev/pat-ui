a6eeec73029817cbbeb162a178ba40f8e318032d
Tieulam Thai <ttl.t954@gmail.com>
2021-04-29 11:39:00 -0600

    add: storybook component

--- src/components/Container/Container.stories.tsx
+++ src/components/Container/Container.stories.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import Container from './Container';
+import { DefaultCard } from '../Card/Card.stories'
 
 export default {
     title: 'Container',
     component: Container,
 }
 
-export const DefaultContainer = () => {
+export const DefaultContainer = () => (
     <Container>
-        <p>child 1</p>
-        <p>child 2</p>
-        <p>child 3</p>
+        <DefaultCard />
+        <DefaultCard />
+        <DefaultCard />
+        <DefaultCard />
+        <DefaultCard />
     </Container>
-}
+)

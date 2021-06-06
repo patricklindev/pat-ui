d03375ccb59f7d3acd563986915910d17cba8ce4
huycans <huycan_lk@yahoo.com.vn>
2021-06-06 16:44:34 -0400

    Reduce snackbar's autoHideDuration to 2secs

--- src/components/Snackbar/Snackbar.tsx
+++ src/components/Snackbar/Snackbar.tsx
@@ -1,5 +1,4 @@
 import React, { FC, useEffect, useState } from 'react';
-import Button from '../Button';
 import { classNames } from '../../utils/classNames';
 
 export type SnackbarSeverity =
@@ -16,11 +15,11 @@ export interface SnackbarProps {
   vertical?: string;
   /** message to display, this is used only if there're no children in snackbar */
   message?: string;
-  /** set duration to hide snackbar */
+  /** after this duration, the snackbar will be hidden*/
   autoHideDuration?: number;
-  /** manage snackbar open/close state with this prop */
+  /** manage snackbar open/close state with this prop; should be passed from a parent component */
   open?: boolean;
-  /** if open is provided, this function should also be provided to set open state to false*/
+  /** if open is provided, this function should also be provided from parent component to set open state to false; if open is provided and onClose is empty, the snackbar will not close */
   onClose?: () => void;
   /** set style of snackbar */
   severity?: SnackbarSeverity;
@@ -39,7 +38,7 @@ export const Snackbar: FC<SnackbarProps> = (props) => {
   let { open, severity, children, message } = props;
   //default value for onClose: empty function
   const onClose = props.onClose || (() => {});
-  const autoHideDuration = props.autoHideDuration || 3000;
+  const autoHideDuration = props.autoHideDuration;
 
   //define class names for snackbar
   let classnames = classNames(
@@ -81,6 +80,7 @@ Snackbar.defaultProps = {
   vertical: 'bottom',
   horizontal: 'left',
   open: true,
+  autoHideDuration: 2000,
 };
 
 export default Snackbar;

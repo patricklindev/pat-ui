d34ece59ba92486600efb0ddf30b8d29076f1390
sankeerthreddy1597 <54692080+sankeerthreddy1597@users.noreply.github.com>
2021-08-03 13:34:53 -0400

    alert componenet and styles

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, {
+    HTMLAttributes,
+    FC,
+} from "react";
+import { classNames } from '../../utils/classNames';
 
-export const Alert = () => {
-    
+export type AlertSize = 'lg' | 'sm';
+
+export type AlertType =
+  | 'primary'
+  | 'secondary'
+  | 'danger'
+  | 'default';
+
+export interface IAlertProps {
+    /** set customized style */
+    className?: string;
+    /** set Alert size */
+    alertSize?: AlertSize;
+    /** set Alert type */
+    alertType?: AlertType;
+}
+
+type PatAlertProps = IAlertProps & HTMLAttributes<HTMLDivElement>;
+
+export const Alert: FC<PatAlertProps> = (props) => {
+    const { alertSize, alertType, children, className, ...rest } = props;
+    let styleClasses = classNames('alert', {
+        [`alert-${alertType}`]: true,
+        [`alert-${alertSize}`]: !!alertSize,
+      });
+      if (className) {
+        styleClasses += ' ' + className;
+      }
+    return (
+        <div
+          className={styleClasses}
+          {...(rest as PatAlertProps)}
+        >
+            {children}
+        </div>
+    )
 } 
+
+Alert.defaultProps = {
+    alertType: 'default',
+  };
 
 export default Alert;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Alert from './components/Alert/Alert';
+import Button from "./components/Button";
+import './styles/index.scss';
+
 export { default as Button } from './components/Button';
 export { default as Input } from './components/Input';
 export { default as Icon } from './components/Icon';
 export { default as Message } from './components/Message';
 export { default as Card } from './components/Card';
 export { default as Dropdown } from './components/Dropdown';
+
+
+
+ReactDOM.render(
+    <>
+      <Alert alertSize="sm">Small Alert</Alert>
+      <Alert>Default Alert</Alert>
+      <Alert alertSize="lg">Large Alert</Alert>
+      <br/>
+      <Alert alertType="primary">Primary Default Alert</Alert>
+      <Alert alertType="secondary">Secondary Default Alert</Alert>
+      <Alert alertType="danger">Danger Default Alert</Alert>
+    </>,
+    document.getElementById("root")
+  );

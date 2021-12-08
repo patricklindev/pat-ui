d2bfdf474cba4a286153699969695f846deef821
Anthony Kuang <AnthonyKuang116@gmail.com>
2021-12-07 23:44:19 -0500

    added mixin.scss for slider

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Slider } from "./index"
+
+export default function App(){
+    console.log("test")
+    return(
+        <div className="App">
+            <h1>Testing</h1>
+            <Slider></Slider>
+        </div>
+    )
+}
+

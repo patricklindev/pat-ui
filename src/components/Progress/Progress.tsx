3165dd5be209fbca1e36ba5d1bf4b23490bff44f
Benson Tsang <df981339@gmail.com>
2021-12-10 07:32:34 -0500

    circular progress bar done

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -14,6 +14,15 @@ export type ProgressColor =
 // progress size
 export type ProgressSize = 'xs' | 'sm' | 'lg' | 'xl';
 
+export interface IRingProps {
+  height: number;
+  width: number;
+  cx: number;
+  cy: number;
+  radius: number;
+  strokeWidth: number;
+}
+
 // props type
 export interface IProgressProps {
   /** set class name */
@@ -59,7 +68,104 @@ export const Progress: FC<IProgressProps> = (props) => {
       </div>
     );
   } else if (pgType === 'circular') {
-    renderComponent = <div className={styleClasses}></div>;
+    let ringProps: IRingProps;
+
+    switch (pgSize) {
+      case 'xs':
+        ringProps = {
+          height: 25,
+          width: 25,
+          cx: 12.5,
+          cy: 12.5,
+          radius: 10,
+          strokeWidth: 2.5,
+        };
+        break;
+
+      case 'sm':
+        ringProps = {
+          height: 50,
+          width: 50,
+          cx: 25,
+          cy: 25,
+          radius: 20,
+          strokeWidth: 5,
+        };
+        break;
+
+      case 'lg':
+        ringProps = {
+          height: 100,
+          width: 100,
+          cx: 50,
+          cy: 50,
+          radius: 40,
+          strokeWidth: 10,
+        };
+        break;
+
+      case 'xl':
+        ringProps = {
+          height: 150,
+          width: 150,
+          cx: 75,
+          cy: 75,
+          radius: 60,
+          strokeWidth: 15,
+        };
+        break;
+
+      default:
+        ringProps = {
+          height: 75,
+          width: 75,
+          cx: 37.5,
+          cy: 37.5,
+          radius: 30,
+          strokeWidth: 7.5,
+        };
+        break;
+    }
+
+    const circumference = ringProps.radius * 2 * Math.PI;
+    const strokeDashoffset = circumference - (pgValue / 100) * circumference;
+
+    renderComponent = (
+      <div className={styleClasses}>
+        <svg
+          className="backgroud"
+          height={ringProps.height}
+          width={ringProps.width}
+        >
+          <circle
+            className="ring"
+            strokeWidth={ringProps.strokeWidth}
+            strokeLinecap="round"
+            fill="transparent"
+            r={ringProps.radius}
+            cx={ringProps.cx}
+            cy={ringProps.cy}
+          />
+        </svg>
+        <svg
+          className="progress"
+          height={ringProps.height}
+          width={ringProps.width}
+        >
+          <circle
+            className="progress-ring"
+            style={{ strokeDashoffset }}
+            strokeWidth={ringProps.strokeWidth}
+            strokeLinecap="round"
+            strokeDasharray={circumference + ' ' + circumference}
+            fill="transparent"
+            r={ringProps.radius}
+            cx={ringProps.cx}
+            cy={ringProps.cy}
+          />
+        </svg>
+      </div>
+    );
   }
 
   return <>{renderComponent}</>;

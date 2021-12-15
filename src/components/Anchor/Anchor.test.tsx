2ae895ab9b7bd0037878d827f432f271c3a605d8
David An <taean2084@gmail.com>
2021-12-14 23:53:04 -0500

    test update

--- src/components/Anchor/Anchor.test.tsx
+++ src/components/Anchor/Anchor.test.tsx
@@ -4,49 +4,43 @@ import { render, fireEvent } from '@testing-library/react';
 import Anchor, { PatAnchorProps } from './Anchor';
 
 describe('Anchor', () => {
-  it('should render deafult link', () => {
+  test('should render deafult anchor', () => {
     const anchorProps = {
-      onClick: jest.fn(),
       href: '#',
     };
     const wrapper = render(<Anchor {...anchorProps}>Default Anchor</Anchor>);
     const element = wrapper.queryByText('Default Anchor') as HTMLElement;
     expect(element).toBeInTheDocument();
     expect(element.tagName).toBe('A');
-    expect(element).toHaveClass('link link-always link-default link-button');
-    expect(anchorProps.onClick).toHaveBeenCalledTimes(0);
-    fireEvent.click(element);
-    expect(anchorProps.onClick).toHaveBeenCalledTimes(1);
+    expect(element).toHaveClass(
+      'anchor anchor-always anchor-default anchor-inherit'
+    );
   });
-  it('should render primary link', () => {
+  test('should render primary anchor', () => {
     const anchorProps: PatAnchorProps = {
       anchorType: 'primary',
-      onClick: jest.fn(),
       href: '#',
     };
     const wrapper = render(<Anchor {...anchorProps}>Primary Anchor</Anchor>);
     const element = wrapper.queryByText('Primary Anchor') as HTMLElement;
     expect(element).toBeInTheDocument();
     expect(element.tagName).toBe('A');
-    expect(element).toHaveClass('link link-always link-primary link-button');
-    expect(anchorProps.onClick).toHaveBeenCalledTimes(0);
-    fireEvent.click(element);
-    expect(anchorProps.onClick).toHaveBeenCalledTimes(1);
+    expect(element).toHaveClass(
+      'anchor anchor-primary anchor-always  anchor-inherit'
+    );
   });
-  it('should render secondary h1 link', () => {
+  test('should render secondary h1 anchor', () => {
     const anchorProps: PatAnchorProps = {
       anchorType: 'secondary',
       variant: 'h1',
-      onClick: jest.fn(),
       href: '#',
     };
     const wrapper = render(<Anchor {...anchorProps}>Secondary Anchor</Anchor>);
     const element = wrapper.queryByText('Secondary Anchor') as HTMLElement;
     expect(element).toBeInTheDocument();
     expect(element.tagName).toBe('A');
-    expect(element).toHaveClass('link link-always link-secondary link-h1');
-    expect(anchorProps.onClick).toHaveBeenCalledTimes(0);
-    fireEvent.click(element);
-    expect(anchorProps.onClick).toHaveBeenCalledTimes(1);
+    expect(element).toHaveClass(
+      'anchor anchor-secondary anchor-always anchor-h1'
+    );
   });
 });

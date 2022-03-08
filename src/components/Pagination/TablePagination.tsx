9945a3519ee3668b494b9becf4edd70cc6e5c432
Fei <fei.zhou.se@outlook.com>
2022-03-07 19:58:21 -0600

    pagination -- usePagination, Pagination, TablePagination

---
+++ src/components/Pagination/TablePagination.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {
+  PaginationProps,
+  usePagination,
+} from '../../utils/hooks/usePagination';
+
+const TablePagination: React.FC<PaginationProps> = (props) => {
+  const { currentPage, onPrev, onNext } = usePagination(props);
+
+  return (
+    <>
+      {/* PAGINATION TO DO */}
+      <div>
+        <button onClick={onPrev}>Prev</button>
+      </div>
+      {currentPage}
+      <div>
+        <button onClick={onNext}>Next</button>
+      </div>
+      {/* PAGINATION TO DO */}
+    </>
+  );
+};
+
+export default TablePagination;
---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,4 @@
+import Pagination from './Pagination';
+import TablePagination from './TablePagination';
+
+export { Pagination, TablePagination };

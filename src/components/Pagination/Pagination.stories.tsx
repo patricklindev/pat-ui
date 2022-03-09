4bd38cc2ac0ae6d3da146621170d27c95caf1124
Yicheng Ye <ye.yicheng123@gmail.com>
2022-03-08 19:04:37 -0500

    css fix; storybook pagination section;

--- src/components/Pagination/Pagination.stories.tsx
+++ src/components/Pagination/Pagination.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Pagination} from "../Pagination";
 import {action} from '@storybook/addon-actions';
+import Input from "../Input";
 
 export default {
     title: 'Pagination',
@@ -14,63 +15,54 @@ export const DefaultPagination = () => (
         <Pagination count={20} page={10} siblingCount={1} size={'default'} disabled={false} color={'secondary'} shape={'round'}/>
     </div>
 );
-//
-// export const TypesPagination = () => (
-//     <div>
-//         <h1>Focus</h1>
-//         <p>An input field can show a user is currently interacting with it.</p>
-//             <Pagination count={20} page={10} siblingCount={1}  />
-//     </div>
-// );
-//
-// export const DisabledErrorPagination = () => (
-//     <div>
-//         <h1>Disabled</h1>
-//         <p>An input field can show that it is disabled.</p>
-//             <Pagination count={20} page={10} siblingCount={1}  />
-//             <br/>
-//         <br/>
-//         <h1>Error</h1>
-//         <p>An input field can show that the data contains errors.</p>
-//             <Pagination count={20} page={10} siblingCount={1}  />
-//
-//     </div>
-// );
-//
-// export const LoadingPagination = () => (
-//     <div>
-//         <h1>Loading</h1>
-//         <p>An icon input field can show that it is currently loading data.</p>
-//             <Pagination count={20} page={10} siblingCount={1}  />
-//         <br/>
-//         <br/>
-//             <Pagination count={20} page={10} siblingCount={1}  />
-//         <br/>
-//         <br/>
-//         <p>An input field can show that it is currently loading data without an icon, too.</p>
-//             <Pagination count={20} page={10} siblingCount={1}  />
-//     </div>
-// );
-//
-// export const IconPagination = () => (
-//     <div>
-//         <h1>ICON</h1>
-//         <p>An input can be formatted with an icon.</p>
-//             <Pagination count={20} page={10} siblingCount={1}  />
-//         <br/>
-//         <br/>
-//         <p>You can position the icon</p>
-//             <Pagination count={20} page={10} siblingCount={1}  />
-//         <br/>
-//         <br/>
-//         <p>You can pass an Icon props object.</p>
-//             <Pagination count={20} page={10} siblingCount={1}  />
-//
-//     </div>
-// );
-//
-// export const SizePagination = () => (
-//     <div>
-//             <Pagination count={20} page={10} siblingCount={1}  />
-//     </div>
-// );
+
+
+export const ColorPagination = () => (
+    <div>
+        <h1>Color</h1>
+        <p>An pagination can vary in color.</p>
+        <Pagination count={20} page={10} siblingCount={1} size={'default'} color={'primary'} shape={'round'}/>
+        <br/>
+        <br/>
+        <Pagination count={20} page={10} siblingCount={1} size={'default'} color={'secondary'} shape={'round'}/>
+    </div>
+);
+
+
+export const ShapePagination = () => (
+    <div>
+        <h1>Shape</h1>
+        <p>An pagination can vary in shape.</p>
+        <Pagination count={20} page={10} siblingCount={1} size={'default'} color={'primary'} shape={'round'}/>
+        <br/>
+        <br/>
+        <Pagination count={20} page={10} siblingCount={1} size={'default'} color={'primary'} shape={'rounded'}/>
+    </div>
+);
+
+
+
+export const DisabledPagination = () => (
+    <div>
+        <h1>Disabled</h1>
+        <p>An pagination that is disabled.</p>
+            <Pagination count={20} page={10} siblingCount={1} size={'default'} disabled={true} color={'secondary'} shape={'round'}/>
+            <br/>
+        <br/>
+    </div>
+);
+
+export const SizePagination = () => (
+    <div>
+        <h1>Size</h1>
+        <p>An pagination can vary in size.</p>
+        <Pagination count={20} page={10} siblingCount={1} size={'sm'} color={'secondary'} shape={'round'}/>
+        <br/>
+        <br/>
+        <Pagination count={20} page={10} siblingCount={1} size={'default'} color={'secondary'} shape={'round'}/>
+        <br/>
+        <br/>
+        <Pagination count={20} page={10} siblingCount={1} size={'lg'} color={'secondary'} shape={'round'}/>
+    </div>
+);
+

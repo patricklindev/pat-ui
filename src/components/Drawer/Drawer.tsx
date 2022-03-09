dc635ff9816653a4cc4d474ee6693380672e9715
Stephen Wuh <swuh87@gmail.com>
2022-03-08 19:25:03 -0500

    backdrop click drawer close sub feature added

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect, useRef } from 'react';
+import { classNames } from '../../utils/classNames';
 import './_Drawer.scss';
 
 // additional features to work on
 // 1. miniVariantDrawer?: boolean;
-
 
 type Anchor = 'top' | 'left' | 'bottom' | 'right';
 
@@ -12,55 +12,79 @@ interface DrawerProps {
   variant?: boolean;
   open?: boolean;
   onClose?: (e?: React.MouseEvent) => void;
+  className?: string;
 }
 
 const Drawer: FC<DrawerProps> = ({
   anchor = 'left',
-  open= false,
+  open = false,
   onClose,
-  children
+  children,
+  className,
 }): JSX.Element => {
 
+  const [isOpen, setIsOpen] = useState<boolean>(open);
+  const [anchorPosition, setAnchorPosition] = useState<string>(anchor)
+  
+
+  let styleClasses = classNames('drawer', {
+    [`drawer-${anchorPosition}`]: true,
+    [`${isOpen ? '' : 'drawer-openStateFalse'}`]: true
+})
+if (className) {
+    styleClasses += ' ' + className;
+}
+  
 
   
+  //click on backdrop to close drawer feature
+
+  let drawerRef = useRef<HTMLDivElement | null>(null);
+
+
+  useEffect(() =>{
+
+    let handler = (e: any): void=>{
+      if (!drawerRef.current?.contains(e.target)){
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handler )
+
+    return ()=>{
+      document.removeEventListener("mousedown", handler)
+    }
+
+  })
 
   return (
-    
-    
-  <aside className={`drawer ${anchor} ${open? "": "openStateFalse"}`}>
+    <aside ref={drawerRef} className={styleClasses}>
       {children}
-  </aside>
-  
-  )
+    </aside>
+  );
 };
 
-export {Drawer};
+export { Drawer };
 
-
-
-
-const CustomDrawer: FC = (): JSX.Element =>{
-
-
-
-  return(
+const CustomDrawer: FC = (): JSX.Element => {
+  return (
     <div>
       <button>Drawer Btn</button>
-      
-      <Drawer anchor='left' open={false}>
-        <h1>email</h1>
-        <h1>contacts</h1>
-        <h1>drafts</h1>
-        <h1>email</h1>
-        <h1>contacts</h1>
-        <h1>drafts</h1>
-        <h1>email</h1>
-        <h1>contacts</h1>
-        <h1>drafts</h1>
-        </Drawer>
-    </div>
-    
-  )
-}
 
-export {CustomDrawer};
+      <Drawer anchor="bottom" open={true}>
+        <h1>email</h1>
+        <h1>contacts</h1>
+        <h1>drafts</h1>
+        <h1>email</h1>
+        <h1>contacts</h1>
+        <h1>drafts</h1>
+        <h1>email</h1>
+        <h1>contacts</h1>
+        <h1>drafts</h1>
+      </Drawer>
+    </div>
+  );
+};
+
+export { CustomDrawer };

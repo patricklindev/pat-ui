c697bd96108589e1551c3cd566758592fed34ccf
shuxiegit config -- global user.email dorisxieshu@gmail.comssh-keygen -t rsa -C dorisxieshu@gmail.com <dorisxieshu@gmail.com>
2022-03-09 17:17:59 -0800

    checkbox color

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,6 +1,7 @@
 import React, { useState, FC, InputHTMLAttributes } from 'react';
-import { classNames } from '../../utils/classNames';
+import { classNames } from '../../utils/classNames'
 import { CheckIcons } from './CheckIcons';
+// import './css&scss/style.css'
 
 export type CheckboxColor = 'primary' | 'secondary' | 'default';
 export type CheckboxSize = 'medium' | 'small';
@@ -11,7 +12,7 @@ export interface ICheckboxProps {
   checkSize?: CheckboxSize;
   icon?: CheckboxIcon;
   label?: string;
-  onChange?: (e?: React.MouseEvent) => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   isChecked?: boolean;
 }
 
@@ -30,49 +31,56 @@ export const Checkbox: FC<NativeCheckboxProps> = (props) => {
   } = props;
   const [checked, setChecked] = useState(false);
 
-  let checkSizeStyle = classNames({
+  let checkSizeStyles = classNames({
     [`checkbox-${checkSize}`]: true,
+  });
+  let checkColorStyle = classNames({
+    [checkColor]: true,
   });
 
   const handleCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
     setChecked(e.target.checked);
+    if (onChange !== undefined) {
+      onChange(e);
+    }
   };
 
   return (
     <>
       <label className="form-control">
-        <span className={`checkbox-container ${checkSizeStyle}`}>
+        <span className={`checkbox-container ${checkSizeStyles}`}>
           <input type="checkbox" onChange={handleCheck} />
           {checked ? (
             <svg
-              className={checkSizeStyle}
+              className={`${checkSizeStyles} ${checkColorStyle}` } 
+
               viewBox={CheckIcons[`${icon}-fill`].viewBox}
               xmlns="http://www.w3.org/2000/svg"
               aria-hidden="true"
               focusable="false"
             >
               <path
-                fill="rgba(0, 0, 0, 0.54)"
                 d={CheckIcons[`${icon}-fill`].path}
               />
             </svg>
           ) : (
             <svg
-              className={checkSizeStyle}
+              className={`${checkSizeStyles} ${checkColorStyle}` } 
               viewBox={CheckIcons[`${icon}-outline`].viewBox}
               xmlns="http://www.w3.org/2000/svg"
               aria-hidden="true"
               focusable="false"
             >
               <path
-                fill="rgba(0, 0, 0, 0.54)"
                 d={CheckIcons[`${icon}-outline`].path}
               />
             </svg>
           )}
-          <span className="ripple-container"></span>
+          <span
+            className={`ripple-container ripple-container-color-${checkColorStyle}`}
+          ></span>
         </span>
-        <span>Checkbox</span>
+        <span>{label}</span>
       </label>
     </>
   );

546fe47b72f335d9bcbb75bf099954b810502aff
Catalina <catalinamcquade@gmail.com>
2022-03-10 15:06:19 -0500

    feat: Value marker changes color with slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -6,57 +6,65 @@ export type Size = 'large' | 'small';
 export type Color = 'slider-red' | 'slider-blue';
 
 interface ISliderProps {
-    SliderSize?: Size;
-    SliderColor?: Color;
+  SliderSize?: Size;
+  SliderColor?: Color;
 }
 
+const Slider: React.FC<ISliderProps> = ({ SliderColor }) => {
+  const [value, setValue] = useState(100);
+  const [showOrHide, setShowOrHide] = useState('');
+  let classNamesList: string[] = [];
 
-const Slider:React.FC<ISliderProps> = ({SliderColor}) => {
-    const [value, setValue] = useState(100)
-    const [showOrHide, setShowOrHide] = useState('')
-    let classNamesList: string[] = [];
-    
-    if(SliderColor === 'slider-red'){
-        classNamesList.push(SliderColor)
-    }
-    else if(SliderColor === 'slider-blue'){
-        classNamesList.push(SliderColor)
-    }
-    
-    useEffect(() => {
-        const sliderValue: HTMLSpanElement | null = document.querySelector("span");
-        if(sliderValue){
-            sliderValue.style.left = (value/2) + "%";
-            console.log(sliderValue.style)
-        }
-    }, [value])
+  if (SliderColor === 'slider-red') {
+    classNamesList.push(SliderColor);
+  } else if (SliderColor === 'slider-blue') {
+    classNamesList.push(SliderColor);
+  }
 
-    const onInputHandle = (e: any) => {
-        setValue(e.target.value);
-        setShowOrHide('show');
+  useEffect(() => {
+    const sliderValue: HTMLSpanElement | null = document.querySelector('span');
+    if (sliderValue) {
+      sliderValue.style.left = value / 2 + '%';
+      console.log(sliderValue.style);
     }
+  }, [value]);
 
-    const onBlurHandle = () => {
-        setShowOrHide('')
-    }
+  const onInputHandle = (e: any) => {
+    setValue(e.target.value);
+    setShowOrHide('show');
+  };
 
-    return(
-        <div className="range">
-        <div className="sliderValue">
-            <span className={showOrHide}>{value}</span>
-        </div>
-        <div className="field">
-            <div className="value left">0</div>
-        <input 
-        className={classNamesList.join(' ')} 
-        type="range" min="10" max="200" value={value} step="1" 
-            onChange={e => onInputHandle(e)}
-            onBlur={onBlurHandle}
-            />
-            <div className="value right">200</div>
-        </div>
+  const onBlurHandle = () => {
+    setShowOrHide('');
+  };
+
+  const valueClassNames = () => {
+    classNamesList.push('sliderValue');
+    return classNamesList.join(' ');
+  };
+
+  return (
+    <div className="range">
+      <div className={valueClassNames()}>
+        {/* <div className="sliderValue"> */}
+        <span className={showOrHide}>{value}</span>
+      </div>
+      <div className="field">
+        <div className="value left">0</div>
+        <input
+          className={classNamesList.join(' ')}
+          type="range"
+          min="10"
+          max="200"
+          value={value}
+          step="1"
+          onChange={(e) => onInputHandle(e)}
+          onBlur={onBlurHandle}
+        />
+        <div className="value right">200</div>
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default Slider;

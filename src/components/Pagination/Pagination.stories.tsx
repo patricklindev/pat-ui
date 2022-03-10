ff4f1150e4b8b1c3595ed186391a8a4d3c4ade6f
Fei <fei.zhou.se@outlook.com>
2022-03-10 13:25:18 -0600

    pagination story fixed

--- src/components/Pagination/Pagination.stories.tsx
+++ src/components/Pagination/Pagination.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pagination } from '../Pagination';
+import Pagination from '../Pagination';
 // import {action} from '@storybook/addon-actions';
 
 export default {

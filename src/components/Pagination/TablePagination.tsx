a006add22a9089fe444a46faf93846e388e92cb4
Fei <fei.zhou.se@outlook.com>
2022-03-14 14:15:27 -0500

    aria hidden

--- src/components/Pagination/TablePagination.tsx
+++ src/components/Pagination/TablePagination.tsx
@@ -95,13 +95,21 @@ const TablePagination: FC<IPaginationProps> = (props) => {
   // render action buttons
   const renderActionBtn: RenderActionFn = (actionType) => {
     const leftArrow = (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <svg
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
         <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>
       </svg>
     );
 
     const rightArrow = (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <svg
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
         <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>
       </svg>
     );

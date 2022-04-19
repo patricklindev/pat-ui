8e3a0d5ce6dd8eecebd330dc8fc04fb5e4565062
Patrick-FRE <patrick.lin@antra.com>
2022-04-19 15:08:20 -0400

    init Text Component

---
+++ src/components/Text/Text.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import Text from './Text';
+
+export default {
+  title: 'Text',
+  component: Text,
+};
+
+export const DefaultButton = () => <Text />;

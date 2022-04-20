b0ddb1cf56d82259f7b90d32a7a67d168765f049
elsavuong <elsavuong@gmail.com>
2022-04-19 22:02:01 -0500

    test commit

---
+++ src/components/Text/Text.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import Text from './Text';
+
+export default {
+  title: 'Text',
+  component: Text,
+};
+export const DefaultText = () => <Text></Text>;

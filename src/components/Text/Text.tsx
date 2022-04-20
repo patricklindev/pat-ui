d25b89bd091b80a321bbca4e3d7a3a1cd758783f
smankow <codarus77@gmail.com>
2022-04-20 15:38:15 -0400

    Tried to get error-icon.svg to work / interface

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { InputHTMLAttributes} from 'react';
 import ErrorIcon from "../../asset/icon/ErrorIcon";
+// import errorIcon from "../../asset/icon/error-icon.svg"
 import { useState } from "react";
-// import "./_Text.scss";
+// import Icon from '../Icon';
 
-function Text(props: any) {
-    const {error, value, ...rest} = props;
+interface ITextProps {
+  error?: boolean
+  value?: string
+}
+export type InputProps = ITextProps & InputHTMLAttributes<HTMLInputElement>;
+
+function Text({error, value, ...rest}: InputProps) {
+    // const {error, value, ...rest} = props;
   let inputClass = "noError";
   if (error) {
     inputClass = "isError";
@@ -15,26 +22,19 @@ function Text(props: any) {
   return (
     <>
       <div className="textbox-container">
-        <p>{props.name}</p>
         <input
           className={"textbox " + inputClass}
-          placeholder={props.placeholder}
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           {...rest}
         />
         {error && <ErrorIcon className="svg-error" />}
+        {/* {error && <img src={errorIcon} className="svg-error" />} */}
+        {/* {error && <Icon name="error" className="svg-error" color="red"/>} */}
+        {/* <img src={errorIcon} className="svg-error"/> */}
       </div>
     </>
   );
 }
 
 export default Text;
-
-// import React from 'react'
-
-// const Text = ()=>{
-//     return <input/>
-// };
-
-// export default Text

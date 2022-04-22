6fbdfd52295c39ce9e711adb21925d1db6ee1d83
Max Lettenberger <maxlettenberger@gmail.com>
2022-04-22 11:41:52 -0500

    add classNames implementation

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -3,20 +3,35 @@
 import errorSvg from '../../asset/icon/error.svg';
 import searchSvg from '../../asset/icon/search.svg';
 import React, { InputHTMLAttributes, FC } from 'react';
+import { classNames } from '../../utils/classNames';
 
 
 interface ITextProps {
   /** set customized error */
   error?: boolean;
+  /** set customized className */
+  className?: string;
 }
+
+
 
 // function Text(props: any) {
 const Text: FC<ITextProps> = (props) => {
-  const { error, ...rest } = props;
+  const { error, className, ...rest } = props;
+
+  let styleClasses = classNames({
+    'inputContainer': !error,
+    'inputContainer-error': !!error
+  });
+
+  if (className) {
+    styleClasses += ' ' + className;
+  }
+
   return (
     <>
       {!error && (
-        <div className="inputContainer">
+        <div className={styleClasses}>
           <input
             placeholder="Search..."
             className="inputField"
@@ -26,7 +41,7 @@ const Text: FC<ITextProps> = (props) => {
         </div>
       )}
       {error && (
-        <div className="inputContainer-error">
+        <div className={styleClasses}>
           <input
             placeholder="Enter valid term"
             className="inputField-error"

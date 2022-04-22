2a78b7b551fc4a4281b45c220129a13c1c30a83d
smankow <codarus77@gmail.com>
2022-04-22 10:03:25 -0400

    Corrected state updates, classnames, -comments

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,37 +1,62 @@
-import React, { InputHTMLAttributes} from 'react';
-import ErrorIcon from "../../asset/icon/ErrorIcon";
-// import errorIcon from "../../asset/icon/error-icon.svg"
-import { useState } from "react";
-// import Icon from '../Icon';
+import React, { useState, useEffect } from 'react';
+import { classNames } from '../../utils/classNames';
 
 interface ITextProps {
+  /** display error styling */
   error?: boolean
+  /** value set by parent */
   value?: string
 }
-export type InputProps = ITextProps & InputHTMLAttributes<HTMLInputElement>;
+type InputProps = ITextProps & React.HTMLAttributes<HTMLInputElement>;
 
-function Text({error, value, ...rest}: InputProps) {
-    // const {error, value, ...rest} = props;
-  let inputClass = "noError";
+function Text({ className, value='', onChange, error, ...rest }: InputProps) {
+  let inputClass;
   if (error) {
-    inputClass = "isError";
+    inputClass = 'isError';
+  }
+
+  let styleClasses = classNames({
+    'textbox': !error,
+    [`textbox ${inputClass}`]: !!error,
+  })
+  if (className) {
+    styleClasses += ' ' + className;
   }
 
   const [inputValue, setInputValue] = useState(value);
+  useEffect(() => setInputValue(value), [value])
+  const changeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.currentTarget.value);
+
+    if (onChange) {
+      onChange(e);
+    }
+  };
 
   return (
     <>
       <div className="textbox-container">
         <input
-          className={"textbox " + inputClass}
+          className={styleClasses}
           value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
+          onChange={changeValue}
           {...rest}
         />
-        {error && <ErrorIcon className="svg-error" />}
-        {/* {error && <img src={errorIcon} className="svg-error" />} */}
-        {/* {error && <Icon name="error" className="svg-error" color="red"/>} */}
-        {/* <img src={errorIcon} className="svg-error"/> */}
+        {error &&
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="svg-error"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>}
       </div>
     </>
   );

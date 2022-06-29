87e6acc78cb029b817c51f6b91d58396fdc4df7c
zhang lihui <1962840289@qq.com>
2022-06-29 14:39:02 -0400

    adding pagination components

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react"
+import { classNames } from '../../utils/classNames';
+
+export interface IPaginationProps {
+    className?: string;
+    count?: number;
+    disabled?: boolean;
+}
+
+export const Pagination: FC<IPaginationProps> = (props: any) => {
+    return (<div></div>)
+}
+
+export default Pagination;

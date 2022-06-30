13be57f282f5e35ef1d7db88362d3b6b20561ec0
Jameson Chen <[jamesonychan@gmail.com]>
2022-06-30 04:04:32 -0400

    added diff size rating to storybook

--- src/components/Rating/Rating.stories.tsx
+++ src/components/Rating/Rating.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { IconSize } from '../Icon/Icon';
 import Rating from './Rating';
 
 export default {
@@ -6,9 +7,23 @@ export default {
   component: Rating,
 };
 
+const ratingStyle: React.CSSProperties = {
+  display: 'flex',
+  alignItems: 'center',
+};
+
+const sizeArr = ['mini', 'small', 'medium', 'large'];
+
 export const DefaultRating = () => <Rating />;
 
 export const DisabledRating = () => <Rating disabled ratingValueControll={3} />;
+
+export const DiffSizeRating = () =>
+  sizeArr.map((item, index) => (
+    <div key={index} style={ratingStyle}>
+      <Rating size={item as IconSize} />
+    </div>
+  ));
 
 export const ReadyOnlyRating = () => (
   <Rating readonly ratingValueControll={3} />

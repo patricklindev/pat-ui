1e070b60e3dc5847498d238c56d55f1611a856b8
Jameson Chen <[jamesonychan@gmail.com]>
2022-06-30 03:20:19 -0400

    optimized it by replacing static value with typed props

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,18 +1,38 @@
-import React, { FC, ReactNode, useState } from 'react';
+import React, { FC, useState } from 'react';
 import Icon from '../Icon';
+import { IconSize } from '../Icon/Icon';
 
-interface IRatingProps {}
+interface IRatingProps {
+  /** set rating to be disabled */
+  disabled?: boolean;
+  /** set number of stars */
+  ratingCount?: number;
+  /** set size of stars */
+  size?: IconSize;
+  /** controll value of filled star ratings */
+  ratingValueControll?: number;
+  /** set label after the rating component */
+  labelInput?: HTMLLabelElement;
+}
 
-const disabled = false;
+/**
+ * Ratings provide insight regarding others' opinions and experiences, and can allow the user to submit a rating of their own.
+ *
+ * ```js
+ * import {Rating} from 'pat-ui'
+ * ```
+ */
 
 export const Rating: FC<IRatingProps> = (props) => {
+  const { disabled, ratingCount, size, ratingValueControll, labelInput } =
+    props;
   // create an arr to map star icons
-  const starArr = [...Array(5)];
+  const starArr = [...Array(ratingCount ?? 5)];
   // states to manage save rating value and hover effect
-  const [ratingNum, setRatingNum] = useState(0);
+  const [ratingNum, setRatingNum] = useState(ratingValueControll ?? 0);
   const [hover, setHover] = useState(0);
 
-  const rating = starArr.map((star, index) => {
+  const rating = starArr.map((_, index) => {
     const ratingValue = index + 1;
     const isFilled = ratingValue <= (hover || ratingNum);
     return (
@@ -32,13 +52,21 @@ export const Rating: FC<IRatingProps> = (props) => {
             name="star"
             color={isFilled ? 'orange' : 'grey'}
             disabled={disabled}
+            size={size}
           />
         </div>
       </label>
     );
   });
 
-  return <>{rating}</>;
+  const label = labelInput && <label>{labelInput}</label>;
+
+  return (
+    <>
+      {rating}
+      {label}
+    </>
+  );
 };
 
 export default Rating;

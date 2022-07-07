cc7970e9220a17e49234444cab056aae5b5543ae
linytas <linyatashi@gmail.com>
2022-07-07 10:20:43 -0400

    test updates(not finished)

--- src/components/Rating/Rating.test.tsx
+++ src/components/Rating/Rating.test.tsx
@@ -8,4 +8,36 @@ describe('Rating', () => {
         const {asFragment} = render(<Rating> Snapshot Message </Rating>);
         expect(asFragment()).toMatchSnapshot();
     });
+
+    it('should be able to preview the rating by hovering on the stars', () => {
+        fail('Needs to be done')
+    });
+
+    it('should be able to disable the component from props', () => {
+        fail('Needs to be done')
+    });
+
+    it('should be able to provide the label of the component from props', () => {
+        fail('Needs to be done')
+    });
+    
+    it('should be able to set the precision of the rating component', () => {
+        fail('Needs to be done')
+    });
+
+    it('should choose sizes of the rating component from props', () => {
+        fail('Needs to be done')
+    });
+
+    it('should decide the number of stars in total from props', () => {
+        fail('Needs to be done')
+    });
+
+    it('should be able to control the value of the rating from outside of the component by providing a prop', () => {
+        fail('Needs to be done')
+    });
+
+    it('should be able to listen to the change of the value of the component from outside of the component by providing the onChange callback function as a prop', () => {
+        fail('Needs to be done')
+    });
 })

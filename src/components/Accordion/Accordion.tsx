00683fd78c34b86d822d769c3e254a3e24e60e00
Kevin Le <kevin@fullcycle.io>
2022-07-13 18:11:02 -0500

    controlled working

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,19 +1,15 @@
-import React, {
-  FC,
-  useState,
-  ReactNode,
-} from 'react';
-import { useEffect } from 'react';
-import './_Accordion.scss'
-
+import React, { FC, useState, ReactNode } from 'react';
+import './_Accordion.scss';
 
 export interface IAccordionProps {
   /** set class name */
   className?: string;
   /** set accordion title */
   title?: ReactNode;
+  /** set accordion id */
+  id?: ReactNode;
   /** set accordion content */
-  content?:  ReactNode;
+  content?: ReactNode;
   /** set accordion expand icon */
   expandIcon?: ReactNode;
   /** set accordion collapse icon */
@@ -21,14 +17,16 @@ export interface IAccordionProps {
   /** set accordion disabled */
   disabled?: boolean;
   /** set accordion expanded by default */
-  expanded?: boolean;
-  /** set accordion expanded by default */
-  controlledExpanded?: boolean;
-  /** listen to expand and collapse */
-  id?: string;
-  onChange?: (expanded: boolean) => void;
-}
+  defExpanded?: boolean;
+  /** set multi accordions */
+  multiple?: boolean;
+  // controlled nums of accordion
+  controlledNum?: number;
 
+  accordionArr?: [];
+
+  onChange: () => void;
+}
 
 export const Accordion: FC<IAccordionProps> = (props) => {
   const {
@@ -39,52 +37,100 @@ export const Accordion: FC<IAccordionProps> = (props) => {
     expandIcon,
     collapseIcon,
     disabled,
-    expanded,
-    controlledExpanded,
+    defExpanded,
+    accordionArr,
+    controlledNum,
+    multiple,
     onChange,
     ...rest
   } = props;
 
-  const [expand, setExpand] = useState(expanded);
+  const createNumsCheckBoxData = (num) => {
+    return new Array(num).fill(0).map((_, index) => {
+      let arrIndex = index;
+      return {
+        id: (Math.random() * 1000).toString(16),
+        isOpen: false,
+        accordionObj: {
+          title: accordionArr.map((item, index) =>
+            index === arrIndex ? item.title : null
+          ),
+          content: accordionArr.map((item, index) =>
+            index === arrIndex ? item.content : null
+          ),
+        },
+      };
+    });
+  };
 
-  const closeAll = () => {
-    setExpand(!expand)
-  }
-  const toggle = (e, controlledExpanded) => {
-    if(controlledExpanded){
-      closeAll()
-    }
-    if (!disabled && !controlledExpanded) {
-      setExpand(!expand);
-      onChange && onChange(!expand);
-    } else if (e && controlledExpanded === true){
-      setExpand(!expand)
-    } else if (!e && controlledExpanded === false){
-      setExpand(expand)
-
-    }
-  }
-
-    return (
-      <div className={`accordion ${disabled && 'disabled'}`} onClick={(e) => toggle(e, controlledExpanded)}>
-      <div className='accordion-header'>
-        <div className="accordion-title">
-          {props.title}
-        </div>
-        <div className={`accordion-icon ${expand ? 'open' : 'close'}`}>
-          {expand ? collapseIcon : expandIcon}
-        </div>
-      </div>
-      <div className={`accordion-content ${expand ? 'show' : 'hidden' }`}>
-        {props.content}
-      </div>
-    </div>
+  const [accordions, setAccordions] = useState(
+    createNumsCheckBoxData(controlledNum)
   );
 
-}
+  const handleClick = (id) => {
+    if (disabled) return;
+    if (multiple) {
+      setAccordions(
+        accordions.map((accordion) => {
+          if (accordion.id === id) {
+            return {
+              ...accordion,
+              isOpen: !accordion.isOpen,
+            };
+          }
+          return {
+            ...accordion,
+            isOpen: false,
+          };
+        })
+      );
+    } else {
+      setAccordions(
+        accordions.map((accordion) => {
+          if (accordion.id === id) {
+            return {
+              ...accordion,
+              isOpen: !accordion.isOpen,
+            };
+          }
+          return accordion;
+        })
+      );
+    }
+    if (onChange) {
+      onChange(accordions);
+    }
+  };
+
+  return accordions.map((item) => {
+    return (
+      <div
+        key={item.id}
+        className={`accordion ${disabled && 'disabled'}`}
+        onClick={() => handleClick(item.id)}
+      >
+        <div className="accordion-header">
+          <div className="accordion-title">{item.accordionObj.title}</div>
+          <div className={`accordion-icon ${item.isOpen ? 'open' : 'close'}`}>
+            {item.isOpen ? collapseIcon : expandIcon}
+          </div>
+        </div>
+        <div className={`accordion-content ${item.isOpen ? 'show' : 'hidden'}`}>
+          {item.accordionObj.content}
+        </div>
+      </div>
+    );
+  });
+};
 
 Accordion.defaultProps = {
   disabled: false,
-}
+  controlledNum: 3,
+  accordionArr: [
+    { title: 'accordion1', content: 'accordion1' },
+    { title: 'accordion2', content: 'accordion2' },
+    { title: 'accordion3', content: 'accordion3' },
+  ],
+};
 
 export default Accordion;

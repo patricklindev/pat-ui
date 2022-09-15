fb42e3456ae84d4410118e61bdb1d443b74aeef4
Toan <votoan410@gmail.com>
2022-09-15 00:54:54 -0500

    set up story book for sample dialog component, will work on some unit test

---
+++ src/components/Dialog/Dialog.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import Dialog from './index';
+export default {
+    title: 'Dialog',
+    component: Dialog,
+  };
+export const SampleDialog = () => (
+    <div>
+        <Dialog open={true}/>
+    </div>
+);
--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -7,13 +7,17 @@ import React, {
     cloneElement,
     ReactNode,
   } from 'react';
+  import { RiCloseLine } from 'react-icons/ri';
 
 
   export interface DialogProps {
     // Boilerplate values for this component, will add more later
     selectedValue?: any;
-    close?: boolean;
+    open?: boolean;
+    classes?: object;
     onClose?: (val: any) => void;
+    onBackdropClick?: (val: any) => void;
+    
  }
 
  // there will be 3 different designs for dialogs: 
@@ -21,11 +25,47 @@ import React, {
  // alert
  // form dialogs
   const Dialog: FC<DialogProps> = (props) => {
-    const {selectedValue, close, onClose} = props;
+    const {selectedValue, open, onClose, onBackdropClick} = props;
     // do logic in following lines
+    const [ visible, setVisible] =useState<boolean>(false);
 
     return (
-        <></>
+        <>
+        <div className="darkBG" onClick={() => setVisible(false)} />
+      <div className="centered">
+        <div className="modal">
+          <div className="modalHeader">
+            <h5 className="heading">Dialog</h5>
+          </div>
+          <button className="closeBtn" onClick={() => setVisible(false)}>
+            <RiCloseLine style={{ marginBottom: '-3px' }} />
+          </button>
+          <div className="modalContent">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto
+          </div>
+          <div className="modalActions">
+            <div className="actionsContainer">
+              <button className="deleteBtn" onClick={() => setVisible(false)}>
+                Delete 
+              </button>
+              <button
+                className="cancelBtn"
+                onClick={() => setVisible(false)}
+              >
+                Cancel 
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+        </>
     )
   }
+
+  Dialog.defaultProps = {
+    open: false,
+  }
+
   export default Dialog;

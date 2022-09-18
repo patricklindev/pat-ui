bbc9791967d2b7f857fce3b666ec59b07214e285
john-chang94 <j.chang94@yahoo.com>
2022-09-18 01:08:25 -0700

    update component logic, icons, & styles

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -12,14 +12,22 @@ type TToastColor =
 type TToastPostion = 'top-right' | 'bottom-right' | 'bottom-left' | 'top-left';
 
 export interface IToastProps {
+  // determines if toast is visible
   open: boolean;
+  // set toast to preset color
   color?: TToastColor;
+  // set toast position on screen
   position?: TToastPostion;
+  // set toast message title
   title: string;
+  // set toast message body
   message?: string;
+  // time in milliseconds for toast to close
   autoHideDuration?: number;
-  icon?: string;
-  onClose?: () => void;
+  // set custom icon in toast; Material Icons support only (https://fonts.google.com/icons?selected=Material+Icons)
+  customIcon?: string;
+  // callback function when user closes the toast
+  onClose: () => void;
 }
 
 export const Toast = ({
@@ -29,41 +37,80 @@ export const Toast = ({
   title,
   message,
   autoHideDuration,
-  icon,
-  onClose
+  customIcon,
+  onClose,
 }: IToastProps) => {
   const [styles, setStyles] = useState('');
+  const [didClose, setDidClose] = useState(false);
+
+  const renderIcon = (color: string) => {
+    if (customIcon) return customIcon;
+    switch (color) {
+      case 'success':
+        return 'done';
+      case 'info':
+        return 'info_outline';
+      case 'warning':
+        return 'warning_amber';
+      case 'danger':
+        return 'highlight_off';
+      default:
+        return;
+    }
+  };
+
+  const handleOnClose = () => {
+    setDidClose(true);
+  };
 
   useEffect(() => {
+    let timer: NodeJS.Timeout;
     if (open) {
-      let styleClasses = classNames({
+      setDidClose(false);
+      let styleClasses = classNames('toast', {
         [`toast__${color}`]: true,
         [`toast__${position}`]: true,
       });
       setStyles(styleClasses);
 
       if (autoHideDuration) {
-        setTimeout(() => {
+        timer = setTimeout(() => {
           setStyles('');
-          // onClose();
+          onClose();
         }, autoHideDuration);
       }
     }
+
+    return () => clearTimeout(timer);
   }, [open]);
 
-  return (
-    <div className={`toast ${styles}`}>
-      <div />
-      <div>
-        <img src='' alt='' />
-      </div>
-      <div>
-        <h6>{title}</h6>
-        <p>{message}</p>
-      </div>
-      <div>
-        <img src='' alt='' />
+  useEffect(() => {
+    if (didClose) {
+      setStyles('');
+      onClose();
+    }
+  }, [didClose]);
+
+  return (open && styles && (
+    <div className={`${styles}`}>
+      <div className={`toast__edge__${color}`}></div>
+      <div className='toast__wrapper'>
+        <div className='toast__inner-wrapper'>
+          <div>
+            {/* <img src=' alt=' /> */}
+            <span className='material-icons toast__icon'>
+              {renderIcon(color)}
+            </span>
+          </div>
+          <div className='toast__text-wrapper'>
+            <h6>{title}</h6>
+            <p>{message}</p>
+          </div>
+        </div>
+        <div onClick={handleOnClose}>
+          <span className='material-icons toast__close-icon'>close</span>
+        </div>
       </div>
     </div>
-  );
+  )) as JSX.Element;
 };

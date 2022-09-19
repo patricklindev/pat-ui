784d2797bcfbd5c565e9bed7893ab4b6be892959
Yusen Jiang <yusenjia@buffalo.edu>
2022-09-18 18:04:56 -0700

    finish switch component

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -2,34 +2,53 @@ import React from 'react';
 import { classNames } from '../../utils/classNames';
 
 export type SwitchSize = 'sm' | 'lg';
-export type SwitchColor = 'primary' | 'secondary';
+export type SwitchColor = 'primary' | 'secondary' | 'default';
 
 export interface ISwitchProps {
+  // set class name
   className?: string;
+  //set switch size
   switchSize?: SwitchSize;
+  //set switch color
   switchColor?: SwitchColor;
+  //set disabled switch
   disabled?: boolean;
+  //set switch label
   label?: string;
+  //set action when clicked
+  onClick?: () => void;
 }
 
 const Switch: React.FC<ISwitchProps> = (props) => {
-  //   const { switchSize, className, disabled, switchColor, label } = props;
+  const { switchSize, className, disabled, switchColor, label } = props;
 
-  //   let styleClasses = classNames('btn', {
-  //     [`btn-${switchColor}`]: true,
-  //     [`btn-${switchSize}`]: !!switchSize,
-  //   });
-  //   if (className) {
-  //     styleClasses += ' ' + className;
-  //   }
+  let styleClasses = classNames('switch', {
+    [`switch-${switchColor}`]: true,
+    [`switch-${switchSize}`]: !!switchSize,
+    disabled: !!disabled,
+  });
+  if (className) {
+    styleClasses += ' ' + className;
+  }
 
   return (
-    <>
-      <label className="switch-label">
-        <input type="checkbox" className="switch-toggle" />
-        <span className="switch-slider"></span>
+    <div className={styleClasses}>
+      <label className="switch-container">
+        <div>
+          <input
+            type="checkbox"
+            className="switch-toggle"
+            //avoid any action when disabled
+            onClick={disabled ? (e) => e.preventDefault() : props.onClick}
+          />
+          <span
+            //set class name based on if switch is disabled or not
+            className={disabled ? 'disabled switch-slider' : 'switch-slider'}
+          />
+        </div>
       </label>
-    </>
+      <p className="switch-label">{label}</p>
+    </div>
   );
 };
 

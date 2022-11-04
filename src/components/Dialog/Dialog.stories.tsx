739e455d25b0d94ce35a925d3c80b140b0d20010
giohax <giovannifgonzaga@gmail.com>
2022-11-04 05:08:31 -0500

    add simple, alerts, and form variant

--- src/components/Dialog/Dialog.stories.tsx
+++ src/components/Dialog/Dialog.stories.tsx
@@ -1,19 +1,86 @@
-import Dialog from './Dialog';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogList,
+  DialogTitle,
+  DialogListItem,
+  DialogListItemAvatar,
+  DialogListItemText,
+} from './Dialog';
 import React from 'react';
 import { DialogProps } from './Dialog';
+import Button from '../Button';
+import Input from '../Input';
+import Message from '../Message';
+import Icon from '../Icon';
 
 export default {
   title: 'Dialog',
   component: Dialog,
 };
 
+const emails = ['username@gmail.com', 'user02@gmail.com'];
+
+export const Simple = () => (
+  <Dialog>
+    <DialogTitle>Set backup account</DialogTitle>
+    <DialogList>
+      {emails.map((email) => (
+        <DialogListItem key={email} handler={() => console.log('clicked user')}>
+          <DialogListItemAvatar>
+            <Icon color="blue" name="heart" />
+          </DialogListItemAvatar>
+          <DialogListItemText>{email}</DialogListItemText>
+        </DialogListItem>
+      ))}
+      <DialogListItem handler={() => console.log('adding account')}>
+        <DialogListItemAvatar>
+          <Icon color="grey" name="plus" />
+        </DialogListItemAvatar>
+        <DialogListItemText>Add account</DialogListItemText>
+      </DialogListItem>
+    </DialogList>
+  </Dialog>
+);
+
 export const Alerts = () => (
-  <Dialog
-    title="Use Google's location service?"
-    content="Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running."
-    actions={[
-      { text: 'AGREE', onClick: () => console.log('AGREE') },
-      { text: 'DISAGREE', onClick: () => console.log('DISAGREE') },
-    ]}
-  ></Dialog>
+  <Dialog>
+    <DialogTitle>Use Google's location service?</DialogTitle>
+    <DialogContent>
+      <DialogContentText>
+        Let Google help apps determine location. This means sending anonymous
+        location data to Google, even when no apps are running.
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button btnSize="sm" btnType="link">
+        DISAGREE
+      </Button>
+      <Button btnSize="sm" btnType="link">
+        AGREE
+      </Button>
+    </DialogActions>
+  </Dialog>
+);
+
+export const Form = () => (
+  <Dialog>
+    <DialogTitle>Subscribe</DialogTitle>
+    <DialogContent>
+      <DialogContentText>
+        To subscribe to this website, please enter your email address here. We
+        will send updates occasionally.
+      </DialogContentText>
+      <Input fluid placeholder="Search..." inputSize="small" />
+    </DialogContent>
+    <DialogActions>
+      <Button btnSize="sm" btnType="link">
+        CANCEL
+      </Button>
+      <Button btnSize="sm" btnType="link">
+        SUBSCRIBE
+      </Button>
+    </DialogActions>
+  </Dialog>
 );
--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -2,26 +2,77 @@ import React, { ReactNode } from 'react';
 import './_Dialog.scss';
 
 export interface DialogProps {
-  title?: string;
-  content?: string;
-  actions?: {
-    text: string;
-    onClick: () => void;
-  }[];
+  children: ReactNode;
 }
 
-const Dialog = ({ title, content, actions }: DialogProps) => {
-  return (
-    <div className="card">
-      <div>{title}</div>
-      <div className="content">{content}</div>
-      <div className="actions">
-        {actions?.map((action) => (
-          <button onClick={action.onClick}>{action.text}</button>
-        ))}
-      </div>
-    </div>
-  );
+interface DialogTitleProps {
+  children: string;
+}
+
+interface DialogContentProps {
+  children?: ReactNode;
+}
+
+interface DialogContentTextProps {
+  children: string;
+}
+
+interface DialogActionsProps {
+  children: ReactNode;
+}
+
+interface DialogListProps {
+  children: ReactNode;
+}
+
+interface DialogListItemProps {
+  handler: () => void;
+  children: ReactNode;
+}
+
+interface DialogListItemAvatarProps {
+  children: ReactNode;
+}
+
+interface DialogListItemTextProps {
+  children: string;
+}
+
+const Dialog = ({ children }: DialogProps) => {
+  return <div className="card">{children}</div>;
+};
+
+export const DialogTitle = ({ children }: DialogTitleProps) => {
+  return <p className="title">{children}</p>;
+};
+
+export const DialogContent = ({ children }: DialogContentProps) => {
+  return <div className="content">{children}</div>;
+};
+export const DialogContentText = ({ children }: DialogContentTextProps) => {
+  return <p>{children}</p>;
+};
+
+export const DialogActions = ({ children }: DialogActionsProps) => {
+  return <div className="actions">{children}</div>;
+};
+
+export const DialogList = ({ children }: DialogListProps) => {
+  return <ul className="list">{children}</ul>;
+};
+
+export const DialogListItem = ({ handler, children }: DialogListItemProps) => {
+  return <li onClick={handler}>{children}</li>;
+};
+
+export const DialogListItemAvatar = ({
+  children,
+}: DialogListItemAvatarProps) => {
+  return <>{children}</>;
+};
+
+export const DialogListItemText = ({ children }: DialogListItemTextProps) => {
+  return <p>{children}</p>;
 };
 
 export default Dialog;

5c3cc591e2309bf2fe6c70efd034ec97a8f6c7a5
giohax <giovannifgonzaga@gmail.com>
2022-11-09 08:12:15 -0600

    add simple, form, alerts test

--- src/components/Dialog/Dialog.test.tsx
+++ src/components/Dialog/Dialog.test.tsx
@@ -1,10 +1,28 @@
 import Dialog from './Dialog';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Simple } from './Dialog.stories';
+import { Simple, Form, Alerts } from './Dialog.stories';
 
-test('renders the simple dialog correctly', () => {
-  render(<Simple />);
-  const title = screen.getByText('Set backup account');
-  expect(title).toBeInTheDocument();
+describe('Simple Dialog', () => {
+  test('renders the prebuilt simple dialog', () => {
+    render(<Simple />);
+    const title = screen.getByText('Set backup account');
+    expect(title).toBeInTheDocument();
+  });
+});
+
+describe('Form Dialog', () => {
+  test('renders the prebuilt form dialog', () => {
+    render(<Form />);
+    const title = screen.getByText('Subscribe');
+    expect(title).toBeInTheDocument();
+  });
+});
+
+describe('Alerts Dialog', () => {
+  test('renders the prebuilt alerts dialog', () => {
+    render(<Alerts />);
+    const title = screen.getByText("Use Google's location service?");
+    expect(title).toBeInTheDocument();
+  });
 });

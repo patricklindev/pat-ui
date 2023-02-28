a9ef40356281a67b63163d42e0e4012bee60bb39
Pengxiang Zhang <z-git@protonmail.com>
2023-02-28 14:45:28 -0500

    Set up dialogue

--- src/components/Dialogue/Dialogue.stories.tsx
+++ src/components/Dialogue/Dialogue.stories.tsx
@@ -1,25 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../Button';
 import Dialogue from './Dialogue';
-
+import Input from '../Input';
 export default {
   title: 'Dialogue',
   component: Dialogue,
 };
 
-export const SimpleDialogue = () => (
-  <div>
-    <Dialogue DialogueType="simple"/>
-  </div>
-);
+export const SimpleDialogue = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      <Button btnType="default" disabled={false} onClick={() => setOpen(true)}>
+        Open Dialog
+      </Button>
 
-export const AlertsDialogue = () => (
-  <div>
-    <Dialogue DialogueType="alerts"/>
-  </div>
-);
+      <Dialogue
+        DialogueType="simple"
+        open={open}
+        onClose={()=>setOpen(false)}
+        dialogueTitle="Set backup account"
+      >
+        <p>Some Account</p>
+      </Dialogue>
+    </div>
+  );
+};
 
-export const FormDialogue = () => (
-  <div>
-    <Dialogue DialogueType="form"/>
-  </div>
-);
+export const AlertsDialogue = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      <Button btnType="default" disabled={false} onClick={() => setOpen(true)}>
+        Open Dialog
+      </Button>
+
+      <Dialogue
+        DialogueType="alerts"
+        open={open}
+        onClose={()=>setOpen(false)}
+        dialogueTitle="Use google's location service?"
+      >
+        <p>
+          Let Google help apps determine location. This means sending anonymous
+          location data to Google, even when no apps are running.
+        </p>
+        <div style={{ textAlign: 'right' }}>
+          <Button
+            btnType="link"
+            disabled={false}
+            onClick={() => setOpen(false)}
+          >
+            Agree
+          </Button>
+          <Button
+            btnType="link"
+            disabled={false}
+            onClick={() => setOpen(false)}
+          >
+            Disagree
+          </Button>
+        </div>
+      </Dialogue>
+    </div>
+  );
+};
+
+export const FormDialogue = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      <Button btnType="default" disabled={false} onClick={() => setOpen(true)}>
+        Open Dialog
+      </Button>
+      <Dialogue
+        DialogueType="form"
+        open={open}
+        onClose={()=>setOpen(false)}
+        dialogueTitle="Subscribe"
+      >
+        <p>
+          To subscribe to this website, please enter your email address here. We will send updates occasionally.
+        </p>
+        <Input fluid placeholder="Email Address" />
+        <div style={{ textAlign: 'right' }}>
+          <Button
+            btnType="link"
+            disabled={false}
+            onClick={() => setOpen(false)}
+          >
+            Cancel
+          </Button>
+          <Button
+            btnType="link"
+            disabled={false}
+            onClick={() => setOpen(false)}
+          >
+            Subscribe
+          </Button>
+        </div>
+      </Dialogue>
+    </div>
+  );
+};
--- src/components/Dialogue/Dialogue.tsx
+++ src/components/Dialogue/Dialogue.tsx
@@ -1,26 +1,19 @@
-import React, { FC, ReactNode } from 'react';
-import Button from '../Button/Button';
+import React, { FC, useEffect, useRef } from 'react';
 import { classNames } from '../../utils/classNames';
 
 export type DialogueType = 'simple' | 'alerts' | 'form';
 
 export interface IDialogueProps {
+  /** set dialogue open */
+  open: boolean;
+  /** set onClose open */
+  onClose: () => void;
   /** set customized dialogue */
   className?: string;
   /** set dialogue mode */
   DialogueType?: DialogueType;
   /** set dialogue title */
   dialogueTitle?: string;
-  /** set dialogue content */
-  dialogueParagraph?: ReactNode;
-  /** set confirmBtn title */
-  confirmBtnTitle?: string;
-  /** set cancelBtn title */
-  cancelBtnTitle?: string;
-  /** set confirmBtn onClick */
-  confirmBtnOnClick?: () => void;
-  /** set cancelBtn onClick */
-  cancelBtnOnClick?: () => void;
 }
 
 export type patDialogueProps = IDialogueProps;
@@ -31,22 +24,79 @@ export type patDialogueProps = IDialogueProps;
  * import {Dialogue} from 'pat-ui'
  * ```
  */
+// []: Developers can provide title, content, actions of the dialog component from children props of the component
+// [x]: Dialog component should have a dimed backdrop by default, users can close the dialog by clicking on the backdrop
+// [x]: Users shouldn’t be able to scroll the page when the dialog is open
+// [x]: Developers should be able to listen to the close of the dialog by providing the onClose callback function.
+// [x]: Developers should be able to control the open/close of the dialog from props.
 export const Dialogue: FC<patDialogueProps> = (props) => {
-  const {
-    className,
-    DialogueType,
-    dialogueTitle,
-    dialogueParagraph,
-    confirmBtnTitle,
-    cancelBtnTitle,
-    confirmBtnOnClick,
-    cancelBtnOnClick,
-    ...rest
-  } = props;
-  let Dialogue = (<div></div>)
+  const { open, onClose, className, dialogueTitle, children } = props;
+  let styleClasses = classNames('dialogue');
+  if (className) {
+    styleClasses += ' ' + className;
+  }
+  const dialogRef = useRef(document.createElement('dialog'));
+
+  // set open/close
+  useEffect(() => {
+    const dialogNode = dialogRef.current;
+    if (open) {
+      dialogNode.showModal();
+    } else {
+      dialogNode.close();
+    }
+  }, [open]);
+
+  // Disable scroll when dialogue is open
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+  }, [open]);
+
+  // Close dialogue when press ESC
+  useEffect(() => {
+    const close = (e: KeyboardEvent) => {
+      if (e.keyCode === 27) {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', close);
+    return () => window.removeEventListener('keydown', close);
+  }, []);
+
+  // Close dialogue when click outside
+  useEffect(() => {
+    dialogRef.current.addEventListener('click', (e) => {
+      const node = e.target as HTMLElement;
+      let rect = node.getBoundingClientRect();
+      if (
+        rect.left > e.clientX ||
+        rect.right < e.clientX ||
+        rect.top > e.clientY ||
+        rect.bottom < e.clientY
+      ) {
+        onClose();
+      }
+    });
+  }, []);
+
+  let Dialogue = (
+      <dialog className={styleClasses} ref={dialogRef}>
+        <div className="dialogue-content">
+          {dialogueTitle && (
+            <div className="dialogue-header">
+              <h4>{dialogueTitle}</h4>
+            </div>
+          )}
+          <div className="dialogue-children">{children}</div>
+        </div>
+      </dialog>
+  );
 
   return Dialogue;
-
 };
 Dialogue.defaultProps = {
   DialogueType: 'simple',

5e1b03aba8b19c1ce1f137f7a5c3407af8fc15d7
Songhan Yu <songhanysh@gmail.com>
2023-05-18 00:32:12 -0400

    finish partial alert component & add icon svg

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -7,12 +7,48 @@ export default {
   component: Alert,
 };
 
-export const DiffTypeAlert = () => {
-  <div>
-    <Alert
-      atType='standard'
-    >
-      Standard Alert
-    </Alert>
-  </div>
-}
+export const DefaultAlert = () => (
+  <>
+    <Alert 
+      atIcon='error'
+      atTextStyle='one-line'
+      atType='standard'>Default Error Alert</Alert>
+
+    <Alert 
+      atIcon='warning'
+      atTextStyle='one-line'
+      atType='standard'>Default Warning Alert</Alert>
+
+  <Alert 
+    atIcon='info'
+    atTextStyle='one-line'
+    atType='standard'>Default info Alert</Alert>
+
+  <Alert 
+    atIcon='success'
+    atTextStyle='one-line'
+    atType='standard'>Default Warning Alert</Alert>
+  </>
+);
+
+// export const DiffSuffixAlert = () => {
+//   <div>
+//     <Alert
+//       atIcon='error'
+//       atSuffix='close-icon'
+//       atTextStyle='one-line'
+//       atType='standard'
+//     >
+//       close-icon suffix Alert
+//     </Alert>
+
+//     <Alert
+//       atIcon='error'
+//       atSuffix='close-icon'
+//       atTextStyle='one-line'
+//       atType='standard'
+//     >
+//       button suffix Alert
+//     </Alert>
+//   </div>
+// }
--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -4,48 +4,30 @@ import Button from '../Button/Button';
 
 export type AlertIcon = 'error' | 'warning' | 'info' | 'success';
 export type AlertSuffix = 'close-icon' | 'button';
-export type AlertText = 'one-line' | 'one-line-bold' | 'two-line' | 'two-line-bold';
+export type AlertTextStyle = 'one-line' | 'one-line-bold' | 'two-line' | 'two-line-bold';
 export type AlertType = 'standard' | 'filled' | 'outlined';
-export type AlertAction = 'text-only' | 'with-button' | 'closable';
 
 export interface AlertProps {
   /** set customized style */
   className?: string;
   atIcon: AlertIcon;
-  atSuffix: AlertSuffix;
-  atText: AlertText;
+  atSuffix?: AlertSuffix;
+  /** set alert text */
+  atTextStyle: AlertTextStyle;
   atType: AlertType;
-  atAction:  AlertAction;
   /** set action on bottun clicked */
   btnOnClick?:() => void;
 }
 
 export const Alert: FC<AlertProps> = (props) => {
-  const {atIcon, atSuffix, atText, atType, atAction, children, className, ...rest} = props;
-  
-  let styleClasses;
-  if (atType == 'standard') {
-    styleClasses = classNames('alert-standard', {
-      [`alert-${atIcon}`]:true,
-      [`alert-${atSuffix}`]:true,
-      [`alert-${atText}`]:true,
-      [`alert-${atAction}`]:true,
-    });
-  } else if (atType == 'filled') {
-    styleClasses = classNames('alert-filled', {
-      [`alert-${atIcon}`]:true,
-      [`alert-${atSuffix}`]:true,
-      [`alert-${atText}`]:true,
-      [`alert-${atAction}`]:true,
-    });
-  } else if (atType == 'outlined') {
-    styleClasses = classNames('alert-outlined', {
-      [`alert-${atIcon}`]:true,
-      [`alert-${atSuffix}`]:true,
-      [`alert-${atText}`]:true,
-      [`alert-${atAction}`]:true,
-    });
-  }
+  const {atIcon, atSuffix, atTextStyle, atType, children, className, ...rest} = props;
+
+  let styleClasses = classNames('alert', {
+    [`alert-${atIcon}`]: true,
+    [`alert-${atSuffix}`]: true,
+    [`alert-${atTextStyle}`]: true,
+    [`alert-${atType}`]: true,
+  });
 
   // add user's custom class
   if (className) {
@@ -54,16 +36,42 @@ export const Alert: FC<AlertProps> = (props) => {
 
   // render different component based on type
   let alertComponent: JSX.Element | undefined;
-  if (atType == 'standard') {
+  if (atType === 'standard') {
     alertComponent = (
-      <div className={styleClasses}  data-testid='alert-element'>
-        
+      <div className={styleClasses} data-testid='stanrdard-element'>
+        <div className="alert-message">{children}</div>
+        {atSuffix === 'button' && (
+          <Button className="alert-action">
+            Button
+          </Button>
+        )}
+        {atSuffix === 'close-icon'}
       </div>
     );
-  } else if (atType == 'filled') {
-
-  } else if (atType == 'outlined') {
-
+  } else if (atType === 'filled') {
+    alertComponent = (
+      <div className={styleClasses} data-testid='filled-element'>
+        <div className="alert-message">{children}</div>
+        {atSuffix === 'button' && (
+          <Button className="alert-action">
+            Button
+          </Button>
+        )}
+        {atSuffix === 'close-icon'}
+      </div>
+    );
+  } else if (atType === 'outlined') {
+    alertComponent = (
+      <div className={styleClasses} data-testid='outlined-element'>
+        <div className="alert-message">{children}</div>
+        {atSuffix === 'button' && (
+          <Button className="alert-action">
+            Button
+          </Button>
+        )}
+        {atSuffix === 'close-icon'}
+      </div>
+    );
   }
 
   return <>{alertComponent}</>;

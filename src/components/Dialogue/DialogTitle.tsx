e4b74e11fb192f4a0548d9f3c7d4197ce23ebb9a
vijay <VijayIyer>
2023-05-18 16:15:36 -0400

    CREATE DIALOG TITLE COMPONENT
    Creates a DialogTitle component which can be used inside a dialog component as a child prop

---
+++ src/components/Dialogue/DialogTitle.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+export interface IDialogTitleProps{
+	/** set style classes*/
+	className?:string;
+}
+export type PatDialogTitleProps = IDialogTitleProps
+const DialogTitle:FC<PatDialogTitleProps> = (props)=>{
+const { children, className } = props;
+	return <h1>{children}</h1>
+}
+export default DialogTitle;

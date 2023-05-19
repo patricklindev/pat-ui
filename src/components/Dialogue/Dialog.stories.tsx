6dbc6b2ecb876c3a63d5b77b823805e69b11b5dd
vijay <VijayIyer>
2023-05-19 08:37:01 -0400

    MODIFY STORIES FILE TO SHOW A FEW DIALOG COMPONENT EXAMPLES
    Modifies stories file to show 3 examples of dialog component usage - SimpleDIalog, Alert Dialog and Form Dialog

--- src/components/Dialogue/Dialog.stories.tsx
+++ src/components/Dialogue/Dialog.stories.tsx
@@ -1,34 +1,145 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { action } from '@storybook/addon-actions';
 import Dialog from './Dialog';
-
+import DialogTitle from './DialogTitle';
+import DialogContent from './DialogContent';
+import DialogActions from './DialogActions';
+import Button from '../Button';
+import Input from '../Input';
+import { CloseDialogReason } from './Dialog';
 export default {
   title: 'Dialogue',
   component: Dialog,
 };
+interface SimpleDialogProps {
+  open: boolean;
+  selectedValue: string;
+  onClose: (value: string) => void;
+}
+function SimpleDialogBody(props: SimpleDialogProps) {
+  const { onClose, open, selectedValue } = props;
+  const emails = ['username@gmail.com', 'user02@gmail.com'];
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
 
-const buttonStyle: React.CSSProperties = {
-  marginRight: '5px',
-  marginTop: '5px',
-};
+  const handleListItemClick = (value: string) => {
+    onClose(value);
+  };
 
-export const SimpleDialog = () => (
-  <>
-    <button>Open Dialog</button>
-    <Dialog>
-      <div>Simple Dialog</div>
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>Set backup account</DialogTitle>
+      <div>
+      {emails.map((email) =>
+          <div>{email}</div>)}
+        <div>Add Account</div>  
+      </div>
     </Dialog>
-  </>
-);
+  );
+}
+export const SimpleDialog = ()=>{
+  const emails = ['username@gmail.com', 'user02@gmail.com'];
+  const [open, setOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(emails[1]);
+   const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-export const AlertDialog = () => (
-  <Dialog>
-    <div>Alert Dialog</div>
-  </Dialog>
-);
+  const handleClose = (value: string) => {
+    setOpen(false);
+    setSelectedValue(value);
+  };
+  return (
+    <>
+      <Button onClick={handleClickOpen}>
+        Open simple dialog
+      </Button>
+      <SimpleDialogBody
+        selectedValue={selectedValue}
+        open={open}
+        onClose={handleClose}
+      />
+    </>
+    )
+}
 
-export const FormDialog = () => (
-  <Dialog>
-    <div>Form Dialog</div>
-  </Dialog>
-);
+export const AlertDialog = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleClickOpen}>
+        Open alert dialog
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          
+            Let Google help apps determine location. This means sending anonymous
+            location data to Google, even when no apps are running.
+          
+        </DialogContent>
+        {/*<DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous
+            location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={handleClose} autoFocus>
+            Agree
+          </Button>
+        </DialogActions> */}
+      </Dialog>
+    </div>
+  );
+}
+
+export const FormDialog = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleClickOpen}>
+        Open form dialog
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          To subscribe to this website, please enter your email address here. We
+            will send updates occasionally.
+          <Input />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}

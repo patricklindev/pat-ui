d95d98bc7a0d0f15e8e4b2ab1133ad30f3a6f337
vijay <VijayIyer>
2023-05-19 08:45:05 -0400

    CREATE DIALOG CONTENT COMPONENT
    Creates DialogContent component to show text or body of the Dialog

---
+++ src/components/Dialogue/DialogContent.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+export interface IDialogContentProps{
+	/** set style classes*/
+	className?:string;
+	/** set dividers option */
+	dividers?:boolean;
+}
+export type PatDialogContentProps = IDialogContentProps
+const DialogContent:FC<PatDialogContentProps> = (props)=>{
+let classes = 'dialog__content';
+const { children, className } = props;
+if(className) classes += ' ' + className;
+	return <div className={classes}>{children}</div>
+}
+export default DialogContent;

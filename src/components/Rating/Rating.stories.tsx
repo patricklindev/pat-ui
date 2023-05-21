ada3fcc8289d9068b30e6843f1091ce3111dde70
zzeng31 <zinuozeng@gmail.com>
2023-05-21 15:28:57 -0700

    finished rating component

--- src/components/Rating/Rating.stories.tsx
+++ src/components/Rating/Rating.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { action } from '@storybook/addon-actions';
 import Rating from './Rating';
 export default {
@@ -6,9 +6,38 @@ export default {
   component: Rating,
 };
 export const DefaultRating = () => {
+  const labels: { [index: string]: string } = {
+    0: 'No Rating',
+    0.5: 'Useless',
+    1: 'Useless+',
+    1.5: 'Poor',
+    2: 'Poor+',
+    2.5: 'Ok',
+    3: 'Ok+',
+    3.5: 'Good',
+    4: 'Good+',
+    4.5: 'Excellent',
+    5: 'Excellent+',
+  };
+  function getLabelText(value: number) {
+    setRating(value);
+    return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+  }
+  const [rating, setRating] = useState<number>(3);
+
+  const handleRatingChange = (value: number) => {
+    setRating(value);
+  };
   return (
     <div>
-      <Rating />
+      {`  Label: ${labels[rating]}, value: ${rating}`}
+      <Rating
+        value={rating}
+        onChange={handleRatingChange}
+        getLabelText={getLabelText}
+        size={'large'}
+        precision={0.5}
+      />
     </div>
   );
 };
--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useRef } from 'react';
 import { classNames } from '../../utils/classNames';
+import { IconPath } from '../Icon/Icons';
 /*
 Users  should be able to preview the rating by hovering on the ‘stars’(proper animation should be applied)
 
@@ -19,9 +20,234 @@ Developers should be able to listen to the change of the value of the component 
 */
 export interface IRatingProps {
   className?: string;
+  name?: string;
+  value: number;
+  defaultValue?: number;
+  readOnly?: boolean;
+  disabled?: boolean;
+  size?: string;
+  max?: number;
+  precision?: number;
+  onChangeActive?: (event: React.SyntheticEvent, value: number) => void;
+  onChange?: (value: number) => void;
+  getLabelText?: (value: number) => string;
 }
 export type patRatingProp = IRatingProps;
+const normalFill = '#FFB400';
+const normalEmpty = 'rgba(0, 0, 0, 0.26)';
+const diabledFill = '#ffda80';
+const disabledEmpty = 'rgba(0, 0, 0, 0.13)';
+interface StarPropsType {
+  starId: number;
+  fill: string;
+  empty: string;
+  fraction: number;
+  className: string;
+  handleRatingChange: (event: React.MouseEvent, value: number) => void;
+  handleMouseHover: (event: React.MouseEvent) => void;
+  handleMouseLeave: (event: React.MouseEvent) => void;
+}
+const Star = ({
+  starId,
+  fill,
+  empty,
+  className,
+  fraction,
+  handleRatingChange,
+  handleMouseHover,
+  handleMouseLeave,
+}: StarPropsType) => {
+  // console.log(`filled:${fraction}%`);
+
+  return (
+    <div
+      id={starId.toString()}
+      onClick={(e) => {
+        // console.log(starId);
+        handleRatingChange(e, starId);
+      }}
+      onMouseMoveCapture={(e) => {
+        handleMouseHover(e);
+      }}
+      // onMouseEnter={(e) => {
+      //   handleMouseEnter(e);
+      // }}
+      onMouseLeave={(e) => {
+        handleMouseLeave(e);
+      }}
+    >
+      <svg viewBox="0 0 24 24" className={className} id={starId.toString()}>
+        <defs>
+          <linearGradient id={`star-${starId}`}>
+            <stop offset="0%" stopColor={fill} />
+            <stop offset={`${fraction}%`} stopColor={fill} />
+            <stop offset={`${fraction}%`} stopColor={empty} />
+            <stop offset="100%" stopColor={empty} />
+          </linearGradient>
+        </defs>
+
+        <path
+          fill={`url(#star-${starId})`}
+          d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
+        ></path>
+      </svg>
+    </div>
+  );
+};
+
 export const Rating: FC<patRatingProp> = (props) => {
-  return <div>This is a rating component</div>;
+  const {
+    className,
+    name,
+    value,
+    defaultValue,
+    precision = 1,
+    readOnly,
+    disabled,
+    size,
+    max,
+    onChangeActive,
+    onChange,
+    getLabelText,
+    ...rest
+  } = props;
+  let styleClasses = classNames('rating_star', {
+    [`rating_star-${size}`]: !!size,
+    [`rating_star-active`]: disabled ? false : true,
+  });
+  if (className) {
+    styleClasses += ' ' + className;
+  }
+  const numberOfStars = max ? max : 5;
+
+  const [rating, setRating] = useState<number>(value ? Math.floor(value) : -1);
+
+  const [lastStarFraction, setLastStarFraction] = useState<number>(
+    value ? Math.round((value - Math.floor(value)) * 100) : 0
+  );
+  const currentRating = useRef<number>(value ? Math.floor(value) : -1);
+  const currentFraction = useRef<number>(
+    value ? Math.round((value - Math.floor(value)) * 100) : 0
+  );
+  const currentTotalRating = useRef<number>(value ? value : 0);
+  const getFraction = (event: React.MouseEvent) => {
+    const { left, width } = event.currentTarget.getBoundingClientRect();
+    const x = event.clientX - left;
+    const fraction = x / width;
+    return fraction;
+  };
+
+  const handleMouseHover = (event: React.MouseEvent) => {
+    const fraction = getFraction(event);
+    if (!disabled) {
+      if (precision === 1) {
+        const id = +event.currentTarget.id;
+        setRating(id);
+        setLastStarFraction(100);
+        currentTotalRating.current = id + 1;
+      } else {
+        const starFraction = Math.floor(fraction / precision) * precision * 100;
+        const nextStarFraction = +(
+          Math.ceil(fraction / precision) *
+          precision *
+          100
+        ).toPrecision(2);
+
+        if (fraction * 100 > starFraction) {
+          setRating(+event.currentTarget.id);
+          setLastStarFraction(nextStarFraction >= 100 ? 100 : nextStarFraction);
+          currentTotalRating.current =
+            nextStarFraction >= 100
+              ? +event.currentTarget.id + 1
+              : +event.currentTarget.id + nextStarFraction / 100;
+        } else {
+          setRating(+event.currentTarget.id);
+          setLastStarFraction(starFraction >= 100 ? 100 : starFraction);
+          currentTotalRating.current =
+            starFraction >= 100
+              ? +event.currentTarget.id + 1
+              : +event.currentTarget.id + starFraction / 100;
+        }
+      }
+      if (getLabelText) {
+        return getLabelText(currentTotalRating.current);
+      }
+    }
+  };
+
+  const handleRatingChange = (event: React.MouseEvent, value: number) => {
+    if (!disabled) {
+      if (onChange) {
+        onChange(value);
+      }
+
+      if (precision === 1) {
+        setRating(value);
+        currentFraction.current = 100;
+        currentRating.current = value;
+        currentTotalRating.current = value + 1;
+      } else {
+        const fraction = getFraction(event);
+        const ceilFraction = Math.ceil(fraction / precision) * precision;
+        const starFraction = Math.floor(ceilFraction * 100);
+
+        setRating(+event.currentTarget.id);
+        setLastStarFraction(starFraction >= 100 ? 100 : starFraction);
+
+        currentFraction.current = starFraction >= 100 ? 100 : starFraction;
+        currentRating.current = +event.currentTarget.id;
+        currentTotalRating.current =
+          starFraction >= 100
+            ? +event.currentTarget.id + 1
+            : +event.currentTarget.id + starFraction / 100;
+      }
+      if (getLabelText) {
+        return getLabelText(currentTotalRating.current);
+      }
+    }
+  };
+
+  const handleMouseLeave = (event: React.MouseEvent) => {
+    // alert(currentRating.current);
+    if (!disabled) {
+      setRating(Math.floor(currentRating.current));
+      setLastStarFraction(currentFraction.current);
+      if (getLabelText) {
+        return getLabelText(
+          Math.floor(currentRating.current) + currentFraction.current / 100
+        );
+      }
+    }
+  };
+  // set the fill and empty color
+  const fill = disabled ? diabledFill : normalFill;
+  const empty = disabled ? disabledEmpty : normalEmpty;
+
+  return (
+    <div className="rating__container">
+      Rating:
+      <div className="rating_number">
+        {lastStarFraction === 100 ? rating + 1 : rating}.
+        {lastStarFraction === 100 ? 0 : lastStarFraction}
+      </div>
+      {Array.from({ length: numberOfStars }).map((_, index) => {
+        const starFraction =
+          rating > index ? 100 : rating === index ? lastStarFraction : 0;
+        return (
+          <Star
+            key={index}
+            starId={index}
+            fill={fill}
+            empty={empty}
+            fraction={starFraction}
+            handleMouseLeave={handleMouseLeave}
+            handleMouseHover={handleMouseHover}
+            handleRatingChange={handleRatingChange}
+            className={styleClasses}
+          />
+        );
+      })}
+    </div>
+  );
 };
 export default Rating;

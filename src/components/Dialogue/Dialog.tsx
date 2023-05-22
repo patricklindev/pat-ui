c4039e414935aa6154ae3727e478a74ba8d85f14
vijay <VijayIyer>
2023-05-22 09:29:00 -0400

    Adds additional required and default props to Dialog component

--- src/components/Dialogue/Dialog.tsx
+++ src/components/Dialogue/Dialog.tsx
@@ -3,7 +3,20 @@ import { classNames } from '../../utils/classNames';
 
 export type DialogCloseReason = 
   | 'backdropClick' 
-  | 'escapeKeyDown'
+  | 'escapeKeyDown';
+
+export type MaxWidthSizeType = 
+'xs'
+| 'sm'
+| 'md'
+| 'lg'
+| 'xl'
+| false
+| string
+
+export type ScrollType = 
+| 'body'
+| 'paper'
 
 export interface IDialogProps {
   /** set customized style */
@@ -12,33 +25,66 @@ export interface IDialogProps {
   open:boolean;
   /** set callback function on closing dialog */
   onClose:(event:object, reason: DialogCloseReason)=>void;
+  /** set aria-describedby to include id of element used to describe component*/
+  ariaDescribedBy?:string;
+  /** set aria-labelledby to include id of element used to label component*/
+  ariaLabelledBy?:string;
+  /** set whether escape key triggers onClose handler */
+  disableEscapeKeyDown?:boolean
+  /** set whether the dialog is fullScreen */
+  fullScreen?:boolean
+  /** set maxWidth of the dialog */
+  maxWidth?:MaxWidthSizeType
+  /** set scroll */
+  scroll?:ScrollType
 }
 
 export type PatDialogProps = IDialogProps
 
 /**
- * A Button indicates a possible user action.
+ * Dialogs inform users about a task and can contain critical information
  *
  * ```js
- * import {Button} from 'pat-ui'
+ * import {Dialog} from 'pat-ui'
  * ```
  */
 export const Dialog: FC<PatDialogProps> = (props) => {
   
-  const { open, onClose, children, className, ...rest } = props;
+  const { open, onClose, disableEscapeKeyDown, children, className, ...rest } = props;
   const handleBackdropClick = (e)=>{
     onClose(e, 'backdropClick')
   }
   const handleEscapeKeyDown = (e) =>{
-    if(e.key==='Escape') onClose(e, 'escapeKeyDown');
+    if(e.key==='Escape') {
+      onClose(e, 'escapeKeyDown');
+    }
   }
+  const handleBackdropScrollingBasedOnDialogState = ()=>{
+    if(open === true) document.body.style.overflow='hidden';
+    else document.body.style.overflow='unset'; 
+  }
+  
+  /** add escape key event listener at the start based on props */
+  useEffect(()=>{
+    if(disableEscapeKeyDown){
+      document.addEventListener('keydown', (e)=>{
+        handleEscapeKeyDown(e);
+      })
+    }
+  }, [])
+
+  /** enable / disable scrolling of backdrop based on whether dialog is open */
+  useEffect(()=>{
+    handleBackdropScrollingBasedOnDialogState();
+  }, [open])
+  
   let styleClasses = 'dialog';
   if (className) styleClasses += ' ' + className;
 
   let dialog;
   dialog = open ? (
-    <div onClick={handleBackdropClick} tab-index="0" onKeyDown={handleEscapeKeyDown} className={styleClasses}>
-      <div onClick={(e)=>{e.stopPropagation()}} className='dialog__body'>
+    <div onClick={handleBackdropClick} className={styleClasses} data-testid='dialog-element'>
+      <div onClick={(e)=>{e.stopPropagation()}} className='dialog__body' data-testid='dialog-body-element'>
         {props.children}
       </div>
     </div>) : <></>;
@@ -46,7 +92,11 @@ export const Dialog: FC<PatDialogProps> = (props) => {
 };
 
 Dialog.defaultProps = {
-  open:true
+  disableEscapeKeyDown:true,
+  maxWidth:'sm',
+  fullScreen:false,
+  fullWidth:false,
+  scroll:'paper'
 };
 
 export default Dialog;

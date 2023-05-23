1cfaa7af8542638f346c266fd89b4ba88ac8568a
JonTimus <timusjohn@gmail.com>
2023-05-23 17:43:09 -0500

    refactored css styles & useCallBack & descriptions to props

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,18 +1,29 @@
-import React, { FC, InputHTMLAttributes, useState } from 'react';
+import React, { FC, InputHTMLAttributes, useState, useCallback } from 'react';
 import { classNames } from '../../utils/classNames';
+// import './_Switch.scss';
 
 export interface ISwitchProps {
+  /** set different colors */
   color?: 'primary' | 'secondary' | 'warning';
-  size?: 'small';
+  /** set small size */
+  size?: string | 'small';
+  /** set label */
   label?: string;
+  /** set checked prop */
   checked?: boolean;
+  /** set onChange prop that returns checked value */
   onChange?: (checked: boolean) => void;
+  /** set disabled prop */
   disabled?: boolean;
+  /** set defaultChecked prop */
   defaultChecked?: boolean;
+  /** set required prop */
   required?: boolean;
+  /** change iconPosition prop */
   iconPosition?: 'left' | 'right';
 }
 
+// Here, we are using InputHTMLAttributes to extend the props of input element
 export type PatSwitchProps = ISwitchProps & InputHTMLAttributes<HTMLInputElement>;
 
 const Switch: FC<PatSwitchProps> = ({
@@ -22,96 +33,48 @@ const Switch: FC<PatSwitchProps> = ({
   checked: checkedProp,
   onChange,
   disabled,
-  defaultChecked,
-  required,
+  defaultChecked = false,
+  required = false,
   iconPosition,
   ...rest
 }) => {
-  const [checked, setChecked] = useState(checkedProp || defaultChecked || false);
+  const [checked, setChecked] = useState(checkedProp || defaultChecked);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newChecked = event.target.checked;
-    setChecked(newChecked);
+  // Here, we are using useCallback to memoize the function to avoid unnecessary re-renders
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const newChecked = event.target.checked;
+      setChecked(newChecked);
 
-    if (onChange) {
-      onChange(newChecked);
-    }
-  };
+      if (onChange) {
+        onChange(newChecked);
+      }
+    },
+    [onChange]
+  );
 
   const styleClasses = {
-    switch: classNames('switch', { checked }),
-    switchLabel: {
-      marginLeft: iconPosition === 'left' ? 0 : 8,
-      marginRight: iconPosition === 'right' ? 0 : 8,
-      display: 'flex',
-      alignItems: 'center',
-      cursor: disabled ? 'not-allowed' : 'pointer',
-      userSelect: 'none',
-    },
-    switchStyle: {
-      position: 'relative',
-      display: 'inline-block',
-      width: size === 'small' ? 36 : 48,
-      height: size === 'small' ? 14 : 20,
-      borderRadius: 16,
-      backgroundColor: checked
-        ? (color === 'primary' && '#1976d2') ||
-          (color === 'secondary' && '#f50057') ||
-          (color === 'warning' && '#ff9800') ||
-          '#bdbdbd'
-        : '#e0e0e0',
-      transition: 'background-color 250ms ease',
-      '&:hover': {
-        backgroundColor: checked
-          ? (color === 'primary' && '#0d47a1') ||
-            (color === 'secondary' && '#d17777') ||
-            (color === 'warning' && '#f57c00') ||
-            '#9e9e9e'
-          : '#bdbdbd',
-      },
-    },
-    thumbStyle: {
-      position: 'absolute',
-      top: '50%',
-      transform: 'translateY(-50%)',
-      left: checked ? (size === 'small' ? 18 : 20) : 0,
-      width: size === 'small' ? 18 : 28,
-      height: size === 'small' ? 18 : 25,
-      borderRadius: '50%',
-      backgroundColor: checked
-        ? (color === 'primary' && '#1976d2') ||
-          (color === 'secondary' && '#d50000') ||
-          (color === 'warning' && '#ff5722') ||
-          '#616161'
-        : '#bdbdbd',
-      boxShadow: checked ? '0 0 3px 3px rgba(0, 0, 0, 0.1)' : 'none',
-      transition: 'left 250ms ease',
-      '&:hover': {
-        left: checked ? (size === 'small' ? 14 : 20) : 0,
-      },
-    },
-    labelCSS: {
-      padding: '0 0 0 8px',
-    },
+    switch: classNames('switch', { checked }, size),
+    thumb: classNames('thumb', size),
+    label: classNames('switch-label-text'),
   };
 
   return (
-    <label style={styleClasses.switchLabel} className="switch-label">
+    <label className="switch-label">
       <input
         type="checkbox"
-        style={{ display: 'none' }}
+        className="switch-input"
         checked={checked}
         onChange={handleChange}
         disabled={disabled}
         defaultChecked={defaultChecked}
         required={required}
-        className="switch-input"
         {...rest}
       />
-      <span style={styleClasses.switchStyle} required className={styleClasses.switch}>
-        <span style={styleClasses.thumbStyle} className="thumb"></span>
+      <span className={styleClasses.switch}>
+        <span className={styleClasses.thumb}></span>
       </span>
-      {label && <span style={styleClasses.labelCSS} className="switch-label-text">{label}</span>}
+      {label && <span className={styleClasses.label}>{label}</span>}
     </label>
   );
 };

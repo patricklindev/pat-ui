35644865549a16c3e332fedbbb4bb262c7a6f9d9
vijay <VijayIyer>
2023-05-24 13:11:25 -0400

    Adds HTMLAttributes type to DialogTitle component

--- src/components/Dialogue/DialogTitle.tsx
+++ src/components/Dialogue/DialogTitle.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from 'react';
-export interface IDialogTitleProps{
-	/** set style classes*/
-	className?:string;
+import React, { FC, HTMLAttributes } from 'react';
+export interface IDialogTitleProps {
+  /** set style classes*/
+  className?: string;
 }
-export type PatDialogTitleProps = IDialogTitleProps
+export type PatDialogTitleProps = IDialogTitleProps &
+  HTMLAttributes<HTMLDivElement>;
 /**
  * Dialog Title can be used in a Dialog to alert users
  *
@@ -11,13 +12,16 @@ export type PatDialogTitleProps = IDialogTitleProps
  * import { DialogTitle } from 'pat-ui'
  * ```
  */
-const DialogTitle:FC<PatDialogTitleProps> = (props)=>{
-	const { children, className } = props;
+const DialogTitle: FC<PatDialogTitleProps> = (props) => {
+  const { children, className } = props;
   let classes;
-  if(className) {
-    classes = `dialog__title `+className;
-  }
-  else classes='dialog__title';
-	return <div className={classes} data-testid='dialog-title-element'>{children}</div>
-}
+  if (className) {
+    classes = 'dialog__title ' + className;
+  } else classes = 'dialog__title';
+  return (
+    <div className={classes} data-testid="dialog-title-element">
+      {children}
+    </div>
+  );
+};
 export default DialogTitle;

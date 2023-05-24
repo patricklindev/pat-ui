1f28a200a486a125a18d21f616c555f29f7cef0a
vijay <VijayIyer>
2023-05-24 13:10:50 -0400

    Adds HTMLAttributes type to DialogContent component

--- src/components/Dialogue/DialogContent.tsx
+++ src/components/Dialogue/DialogContent.tsx
@@ -1,21 +1,35 @@
-import React, { FC } from 'react';
-export interface IDialogContentProps{
-	/** set style classes*/
-	className?:string;
-	/** set dividers option */
-	dividers?:boolean;
-}
-export type PatDialogContentProps = IDialogContentProps
-const DialogContent:FC<PatDialogContentProps> = (props)=>{
-let classes = 'dialog__content';
-const { dividers, children, className } = props;
-if(className) classes += ' ' + className;
-	return dividers ? 
-	(<div className={classes} data-testid='dialog-content-element'><hr />{children}<hr /></div>) :
-	(<div className={classes} data-testid='dialog-content-element'>{children}</div>)
+import React, { FC, HTMLAttributes } from 'react';
+export interface IDialogContentProps {
+  /** set style classes*/
+  className?: string;
+  /** set dividers option */
+  dividers?: boolean;
 }
 
+const divider: FC = () => {
+  return <hr />;
+};
+export type PatDialogContentProps = IDialogContentProps &
+  HTMLAttributes<HTMLDivElement>;
+const DialogContent: FC<PatDialogContentProps> = (props) => {
+  let classes = 'dialog__content';
+  const { dividers, children, className } = props;
+
+  if (className) classes += ' ' + className;
+  return dividers ? (
+    <div className={classes} data-testid="dialog-content-element">
+      <hr className="dialog-content-divider" />
+      {children}
+      <hr className="dialog-content-divider" />
+    </div>
+  ) : (
+    <div className={classes} data-testid="dialog-content-element">
+      {children}
+    </div>
+  );
+};
+
 DialogContent.defaultProps = {
-	dividers:false
-}
+  dividers: false,
+};
 export default DialogContent;

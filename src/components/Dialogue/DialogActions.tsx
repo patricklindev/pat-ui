6a681a7fbffb2990d462f68b81c46f90bfd451a3
vijay <VijayIyer>
2023-05-24 13:09:25 -0400

    Handles disableSpacing prop

--- src/components/Dialogue/DialogActions.tsx
+++ src/components/Dialogue/DialogActions.tsx
@@ -1,22 +1,30 @@
-import React, { FC } from 'react';
-export interface IDialogActionsProps{
-	/** set style classes*/
-	className?:string;
-	/** set whether to space child components */
-	disableSpacing?:boolean;
+import React, { FC, HTMLAttributes } from 'react';
+export interface IDialogActionsProps {
+  /** set style classes*/
+  className?: string;
+  /** set whether to space child components */
+  disableSpacing?: boolean;
 }
-export type PatDialogActionsProps = IDialogActionsProps
+export type PatDialogActionsProps = IDialogActionsProps &
+  HTMLAttributes<HTMLDivElement>;
 
-const DialogActions:FC<PatDialogActionsProps> = (props)=>{
-const { disableSpacing, children, className } = props;
+const DialogActions: FC<PatDialogActionsProps> = (props) => {
+  const { disableSpacing, children, className } = props;
 
-let dialogActions = <div className={`dialog__actions `+className} data-testid='dialog-actions-element'>{children}</div>
-if(disableSpacing) {
-	dialogActions.style.gap = 0;
-}
-return dialogActions;
-}
+  let dialogActions = (
+    <div
+      className={`dialog__actions ${className} ${
+        !disableSpacing && '--dialog__actions-gap'
+      }`}
+      data-testid="dialog-actions-element"
+    >
+      {children}
+    </div>
+  );
+
+  return dialogActions;
+};
 DialogActions.defaultProps = {
-	disableSpacing:false
-}
+  disableSpacing: false,
+};
 export default DialogActions;

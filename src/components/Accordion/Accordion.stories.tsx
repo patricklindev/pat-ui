9ca3a9dbfe0587845c5aebf89f567a1e60287523
zack129 <zewen.shan@authright.com>
2023-07-02 14:17:43 -0400

    finished scss, just need unit test

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
 import Accordion from './Accordion';
-import  heart  from '../../asset/icon/heart-solid.svg';
+import heart from '../../asset/icon/heart-solid.svg';
 export default {
   title: 'Accordion',
   component: Accordion,
@@ -11,27 +10,27 @@ export const DefaultAccordion = () => {
   return (
     <div>
       <h3>Simple Expansion Panel</h3>
-      <Accordion expanded title='Expansion Panel 1'>
-        More RVs were seen in the storage lot than at the campground..
-        More RVs were seen in the storage lot than at the campground..
-        More RVs were seen in the storage lot than at the campground..
+      <Accordion expanded title="Expansion Panel 1">
+        More RVs were seen in the storage lot than at the campground.. More RVs
+        were seen in the storage lot than at the campground.. More RVs were seen
+        in the storage lot than at the campground..
       </Accordion>
-      <Accordion title='Expansion Panel 2' >
-        More RVs were seen in the storage lot than at the campground..
-        More RVs were seen in the storage lot than at the campground..
+      <Accordion title="Expansion Panel 2">
+        More RVs were seen in the storage lot than at the campground.. More RVs
+        were seen in the storage lot than at the campground..
       </Accordion>
-      <Accordion disabled title='Disabled Expansion Panel' expandIcon={heart}  >
-        Behind the window was a reflection that only instilled fear.
-        More RVs were seen in the storage lot than at the campground..
-        More RVs were seen in the storage lot than at the campground..
-        More RVs were seen in the storage lot than at the campground..
+      <Accordion disabled title="Disabled Expansion Panel" expandIcon={heart}>
+        Behind the window was a reflection that only instilled fear. More RVs
+        were seen in the storage lot than at the campground.. More RVs were seen
+        in the storage lot than at the campground.. More RVs were seen in the
+        storage lot than at the campground..
       </Accordion>
     </div>
   );
 };
 
 export const ControlledAccordion = () => {
-  const [expanded, setExpanded] = React.useState<string>('');
+  const [expanded, setExpanded] = React.useState<string>('panel3');
 
   const handleChange = (panel: string) => {
     expanded !== panel ? setExpanded(panel) : setExpanded('');
@@ -43,32 +42,31 @@ export const ControlledAccordion = () => {
       <Accordion
         expanded={expanded === 'panel1'}
         onChange={() => handleChange('panel1')}
-        title='General settings'
-        summary='I am an expansion panel'
+        title="General settings"
+        summary="I am an expansion panel"
       >
         More RVs were seen in the storage lot than at the campground..
       </Accordion>
       <Accordion
         expanded={expanded === 'panel2'}
         onChange={() => handleChange('panel2')}
-        title='Users'
-        summary='You are currently not an owner'
+        title="Users"
+        summary="You are currently not an owner"
       >
         More RVs were seen in the storage lot than at the campground..
       </Accordion>
       <Accordion
         expanded={expanded === 'panel3'}
         onChange={() => handleChange('panel3')}
-        title='Advanced settings'
-        summary='Filtering has been entirely disabled for whole web server'
+        title="Advanced settings"
+        summary="Filtering has been entirely disabled for whole web server"
       >
         Behind the window was a reflection that only instilled fear.
       </Accordion>
       <Accordion
         expanded={expanded === 'panel4'}
         onChange={() => handleChange('panel4')}
-        title='Personal data'
-        
+        title="Personal data"
       >
         Behind the window was a reflection that only instilled fear.
       </Accordion>
@@ -80,20 +78,20 @@ export const AdditionalActions = () => {
   return (
     <div>
       <h3>Simple Expansion Panel</h3>
-      <Accordion type='checkbox'>
-        More RVs were seen in the storage lot than at the campground..
-        More RVs were seen in the storage lot than at the campground..
-        More RVs were seen in the storage lot than at the campground..
+      <Accordion type="checkbox">
+        More RVs were seen in the storage lot than at the campground.. More RVs
+        were seen in the storage lot than at the campground.. More RVs were seen
+        in the storage lot than at the campground..
       </Accordion>
-      <Accordion type='checkbox' >
-        More RVs were seen in the storage lot than at the campground..
-        More RVs were seen in the storage lot than at the campground..
+      <Accordion type="checkbox">
+        More RVs were seen in the storage lot than at the campground.. More RVs
+        were seen in the storage lot than at the campground..
       </Accordion>
-      <Accordion type='checkbox'  >
-        Behind the window was a reflection that only instilled fear.
-        More RVs were seen in the storage lot than at the campground..
-        More RVs were seen in the storage lot than at the campground..
-        More RVs were seen in the storage lot than at the campground..
+      <Accordion type="checkbox">
+        Behind the window was a reflection that only instilled fear. More RVs
+        were seen in the storage lot than at the campground.. More RVs were seen
+        in the storage lot than at the campground.. More RVs were seen in the
+        storage lot than at the campground..
       </Accordion>
     </div>
   );
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,9 +1,8 @@
-import React, { FC, useState, InputHTMLAttributes } from 'react';
+import React, { FC, useState } from 'react';
 import { action } from '@storybook/addon-actions';
-import test from './icon/expanded-down.svg'
-import  up  from '../../asset/icon/expanded-up.svg';
-import  down  from '../../asset/icon/expanded-down.svg';
-import { type } from 'os';
+import up from '../../asset/icon/expanded-up.svg';
+import down from '../../asset/icon/expanded-down.svg';
+import { classNames } from '../../utils/classNames';
 export type AccordionType = 'default' | 'checkbox';
 export interface IAccordionProps {
   /** set customized style */
@@ -15,66 +14,127 @@ export interface IAccordionProps {
   /** set action on title clicked */
   onChange?: () => void;
   /**set Accordion title */
-  title?: string
+  title?: string;
   /**set Accordion summary */
-  summary?: string
+  summary?: string;
   /**set Accordion expand icon */
-  expandIcon?: string
+  expandIcon?: string;
   /**set Accordion retract icon */
-  retractIcon?: string
+  retractIcon?: string;
   /**set Accordion type */
-  type?: AccordionType
+  type?: AccordionType;
 }
 export type patAccordionProps = IAccordionProps;
 export const Accordion: FC<patAccordionProps> = (props) => {
-  const { expanded, disabled, title,summary,onChange,type, ...rest } = props;
+  const {
+    className,
+    expanded,
+    disabled,
+    title,
+    summary,
+    onChange,
+    type,
+    ...rest
+  } = props;
   let initialState: boolean = expanded === undefined ? false : expanded;
   const [show, setShow] = useState<boolean | false>(initialState);
   const handleDefaultClick = () => {
     setShow(!show);
   };
-  const isExpanded:string = show ? 'isExpanded'  : ''
-  const isDisabled:string = disabled ? 'isDisabled' : ''
-  const isControlExpanded:string = props.expanded ? 'isExpanded' : ''
-  // const title = props.title !== undefined ? props.title : 'Need A Title'
+  const isExpanded: string = show ? 'isExpanded' : '';
+  const isDisabled: string = disabled ? 'isDisabled' : '';
+  const isControlExpanded: string = props.expanded ? 'isExpanded' : '';
+
+  let styleClasses = classNames('accordion', {
+    isDisabled: !!isDisabled,
+  });
+  if (className) {
+    styleClasses += ' ' + className;
+  }
+
   let accordion;
-  console.log(type=== 'checkbox')
   if (props.onChange === undefined) {
     accordion = (
-      <div className={`accordion ${isExpanded} ${isDisabled}`}>
-        <div className="accordion title-container">
-          {type === 'checkbox' &&  <input  className="title-checkbox" type={type}/>}
-          <button 
-            className={`accordion title ` }
+      <div className={styleClasses + ' ' + isExpanded}>
+        <div className={styleClasses + ' accordion-title-container'}>
+          {type === 'checkbox' && (
+            <input
+              className={styleClasses + ' accordion-title-checkbox'}
+              type={type}
+            />
+          )}
+          <button
+            className={accordion + ' accordion-title'}
             onClick={handleDefaultClick}
             disabled={disabled}
           >
-            <div className="accordion title-summary">
-              <p className="accordion title-information">{title}</p>   
-              <p className="accordion title-additional">{summary}</p>
-              <img className="accordion title-icon" src={isExpanded ? props.retractIcon: props.expandIcon} alt='mySvgImage' />
+            <div className={styleClasses + ' accordion-title-summary'}>
+              <p className={styleClasses + ' accordion-title-information'}>
+                {title}
+              </p>
+              <p className={styleClasses + ' accordion-title-additional'}>
+                {summary}
+              </p>
+              <img
+                className={styleClasses + ' accordion-title-icon'}
+                src={isExpanded ? props.retractIcon : props.expandIcon}
+                alt="mySvgImage"
+              />
             </div>
           </button>
-        </div >
-        
+        </div>
 
-        {show ?  <p className="accordion accordion-description">{props.children}</p> : <p className="accordion accordion-description hidden">{props.children}</p>}
+        {show ? (
+          <p className={styleClasses + ' accordion-description'}>
+            {props.children}
+          </p>
+        ) : (
+          <p className={styleClasses + ' accordion-description hidden'}>
+            {props.children}
+          </p>
+        )}
       </div>
     );
   } else {
     accordion = (
-      <div  className={`accordion ${isControlExpanded} ${isDisabled}`}>
-        <div className="accordion title-container">
-          {type === 'checkbox' &&  <input  className="title-checkbox" type={type}/>}
-          <button className={`accordion title ` } onClick={onChange} disabled={disabled}><div className="accordion title-summary">
-             <p className="accordion title-information">{title}</p>
-              <p className="accordion title-additional">{summary}</p>
-             <img className="accordion title-icon" src={isExpanded ? props.retractIcon: props.expandIcon} alt='mySvgImage' />
+      <div className={styleClasses + ' ' + isControlExpanded}>
+        <div className={styleClasses + ' accordion-title-container'}>
+          {type === 'checkbox' && (
+            <input
+              className={styleClasses + ' accordion-title-checkbox'}
+              type={type}
+            />
+          )}
+          <button
+            className={accordion + ' accordion-title'}
+            onClick={onChange}
+            disabled={disabled}
+          >
+            <div className={styleClasses + ' accordion-title-summary'}>
+              <p className={styleClasses + ' accordion-title-information'}>
+                {title}
+              </p>
+              <p className={styleClasses + ' accordion-title-additional'}>
+                {summary}
+              </p>
+              <img
+                className={styleClasses + ' accordion-title-icon'}
+                src={isExpanded ? props.retractIcon : props.expandIcon}
+                alt="mySvgImage"
+              />
             </div>
-        </button>
-        </div >
-        
-        {props.expanded ? <p className="accordion accordion-description">{props.children}</p> : <p className="accordion accordion-description hidden">{props.children}</p>}
+          </button>
+        </div>
+
+        {props.expanded ? (
+          <p className={styleClasses + ' accordion-description'}>
+            {props.children}
+          </p>
+        ) : (
+          <p className={styleClasses + ' accordion-description hidden'}>
+            {props.children}
+          </p>
+        )}
       </div>
     );
   }
@@ -82,11 +142,11 @@ export const Accordion: FC<patAccordionProps> = (props) => {
 };
 
 Accordion.defaultProps = {
-  title:'Summary text',
-  summary:"",
-  expandIcon:down,
-  retractIcon:up,
-  type:"default",
+  title: 'Summary text',
+  summary: '',
+  expandIcon: down,
+  retractIcon: up,
+  type: 'default',
 };
 
 export default Accordion;
